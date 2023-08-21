7725adb10719dee617c306a2406d1dc29684718d
dobroillya <dobroillya@gmail.com>
2023-08-21 16:37:22 +0300

    bt-17: * const to enum

--- mobile/src/bundles/common/styles/colors.ts
+++ mobile/src/bundles/common/styles/colors.ts
@@ -1,13 +1,13 @@
-const Color = {
-    PRIMARY: '#274F8D',
-    SECONDARY: '#18A0FB',
-    ERROR: '#FF2121',
-    HOVER: '#0084DD',
-    BACKGROUND: '#F7F8FC',
-    INPUT: '#D5DCE8',
-    TEXT2: '#90A3BF',
-    TEXTDARK: '#7C8798',
-    TAG: '#DCE5FF',
-} as const;
+enum Color {
+    PRIMARY = '#274F8D',
+    SECONDARY = '#18A0FB',
+    ERROR = '#FF2121',
+    HOVER = '#0084DD',
+    BACKGROUND = '#F7F8FC',
+    INPUT = '#D5DCE8',
+    TEXT2 = '#90A3BF',
+    TEXTDARK = '#7C8798',
+    TAG = '#DCE5FF',
+}
 
 export { Color };
