f2b2e916dc6d0b420fb1f4bdaccc97d8735efa6a
JoshDagat <josuer.online.work@gmail.com>
2023-08-21 17:55:32 +0800

    bt-3: + Add aditional options for reusable button component

--- frontend/src/bundles/common/components/button/button.tsx
+++ frontend/src/bundles/common/components/button/button.tsx
@@ -1,19 +1,37 @@
-import { Button } from '@mui/material';
+import { Button, type SxProps, type Theme } from '@mui/material';
 
 type Properties = {
     label: string;
     variant?: 'text' | 'outlined' | 'contained';
     type?: 'submit' | 'button';
+    disabled?: boolean;
+    className?: string;
+    sx?: SxProps<Theme>;
+    endIcon?: React.ReactNode;
+    startIcon?: React.ReactNode;
 };
 
 const CustomButton: React.FC<Properties> = ({
     variant = 'contained',
     label,
     type = 'button',
+    disabled = false,
+    className = '',
+    sx = [],
+    endIcon = null,
+    startIcon = null,
 }) => (
-    <Button type={type} variant={variant}>
+    <Button
+        type={type}
+        variant={variant}
+        disabled={disabled}
+        className={className}
+        sx={[...(Array.isArray(sx) ? sx : [sx])]}
+        endIcon={endIcon}
+        startIcon={startIcon}
+    >
         {label}
-    </Button> //TODO: Add other settings to a button, such as custom styles, color, etc
+    </Button>
 );
 
 export { CustomButton };
