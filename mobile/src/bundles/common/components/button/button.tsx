be5b5794899124940ebbb60b87b0328564f86a9d
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-08-21 15:41:20 +0300

    bt-15: + custom button

--- mobile/src/bundles/common/components/button/button.tsx
+++ mobile/src/bundles/common/components/button/button.tsx
@@ -1,19 +1,73 @@
+import { type IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { type Props } from '@fortawesome/react-native-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React from 'react';
 import { Pressable } from 'react-native';
+import {
+    type PressableProps,
+    type StyleProp,
+    type ViewStyle,
+} from 'react-native';
 
 import { Text } from '~/bundles/common/components/components';
 
 import { styles } from './styles';
 
 type Properties = {
-    label: string;
-    onPress: () => void;
-};
+    label?: string;
+    isPrimary?: boolean;
+    labelStyles?: StyleProp<ViewStyle>;
+    icon?: IconDefinition;
+    iconProps?: Omit<Props, 'icon'>;
+} & PressableProps;
 
-const Button: React.FC<Properties> = ({ label, onPress }) => {
+type StyleRecord = Record<string, unknown>;
+
+const Button: React.FC<Properties> = ({
+    label = '',
+    labelStyles,
+    style,
+    isPrimary = true,
+    disabled = false,
+    icon,
+    iconProps,
+    ...properties
+}) => {
+    const isPrimaryDisabled = isPrimary && disabled && !icon;
+    const isSecondaryDisabled = !isPrimary && disabled && !icon;
+    const isIconDisabled = icon && disabled;
+
+    const calculateButtonStyle = ({
+        pressed,
+    }: {
+        pressed: boolean;
+    }): StyleRecord[] => {
+        const isPrimaryPressed = isPrimary && pressed && !icon;
+        const isSecondaryPressed = !isPrimary && pressed && !icon;
+        const isIconPressed = pressed && icon;
+        const stylesData = [
+            styles.btn,
+            isPrimary ? styles.button_primary : styles.button_secondary,
+            icon && styles.button_with_icon,
+            isPrimaryDisabled && styles.button_primary_disabled,
+            isSecondaryDisabled && styles.button_secondary_disabled,
+            isIconDisabled && styles.button_with_icon_disabled,
+            isPrimaryPressed && styles.button_primary_pressed,
+            isSecondaryPressed && styles.button_secondary_pressed,
+            isIconPressed && styles.button_with_icon_pressed,
+            style,
+        ];
+        return stylesData.filter(Boolean) as StyleRecord[];
+    };
+
     return (
-        <Pressable style={styles.btn} onPress={onPress}>
-            <Text style={styles.label}>{label}</Text>
+        <Pressable
+            {...properties}
+            disabled={disabled}
+            style={calculateButtonStyle}
+        >
+            {icon && <FontAwesomeIcon size={15} icon={icon} {...iconProps} />}
+            <Text style={(styles.label, labelStyles)}>{label}</Text>
         </Pressable>
     );
 };
--- mobile/src/bundles/common/components/button/styles.ts
+++ mobile/src/bundles/common/components/button/styles.ts
@@ -1,14 +1,37 @@
 import { StyleSheet } from 'react-native';
 
+const primaryButton = {};
+const primaryButtonPressed = {};
+const secondaryButtonPressed = {};
+const secondaryButton = {};
+const primaryButtonDisabled = {};
+const secondaryButtonDisabled = {};
+const buttonWithIcon = {
+    flexDirection: 'row' as const,
+    justifyContent: 'center' as const,
+    gap: 10,
+};
+const buttonWithIconDisabled = {};
+const buttonWithIconPressed = {};
+
 const styles = StyleSheet.create({
     btn: {
-        padding: 10,
-        backgroundColor: 'blue',
+        paddingVertical: 15,
+        paddingHorizontal: 37,
         alignItems: 'center',
     },
     label: {
         color: 'white',
     },
+    button_primary: primaryButton,
+    button_secondary: secondaryButton,
+    button_primary_pressed: primaryButtonPressed,
+    button_secondary_pressed: secondaryButtonPressed,
+    button_primary_disabled: primaryButtonDisabled,
+    button_secondary_disabled: secondaryButtonDisabled,
+    button_with_icon: buttonWithIcon,
+    button_with_icon_disabled: buttonWithIconDisabled,
+    button_with_icon_pressed: buttonWithIconPressed,
 });
 
 export { styles };
