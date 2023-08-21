7f5b1b0fdbcb533594a6b0270e25c864c12fc470
Matvey V <main.monyster@gmail.com>
2023-08-21 23:14:37 +0300

    bt-22: * new realization of FormControl

--- frontend/src/bundles/common/components/form-control/form-control.tsx
+++ frontend/src/bundles/common/components/form-control/form-control.tsx
@@ -1,11 +1,5 @@
-import {
-    FilledInput,
-    FormControl as MUIFormControl,
-    FormHelperText,
-    Input,
-    InputLabel,
-    OutlinedInput,
-} from '@mui/material';
+import { type SxProps, type Theme } from '@mui/material';
+import { FormControl as MUIFormControl } from '@mui/material';
 
 const InputVariantTypes = {
     FILLED: 'filled',
@@ -17,94 +11,36 @@ type FormControlVariants =
     (typeof InputVariantTypes)[keyof typeof InputVariantTypes];
 
 type Properties = {
-    name: string;
+    children: React.ReactNode;
     color?: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning'; // TODO: change after the color scheme is merged
     variant?: FormControlVariants;
     isError?: boolean;
     isDisabled?: boolean;
 
-    label?: string;
-    value?: string;
-    helperText?: string;
-    inputLabelProps?: React.ComponentProps<typeof InputLabel>;
-    formHelperTextProps?: React.ComponentProps<typeof FormHelperText>;
+    className?: string;
+    sx?: SxProps<Theme>;
 };
 
 const FormControl: React.FC<Properties> = ({
-    name,
+    children,
     color = 'primary',
+
     variant = 'standard',
     isError = false,
     isDisabled = false,
-    label = '',
-    value = '',
-    helperText = '',
-    inputLabelProps,
-    formHelperTextProps,
-}) => {
-    const formInputId = `${name}-form-input`;
-    const formHelperTextId = `${name}-form-helper-text`;
-
-    const renderVariantInput = (
-        variant: FormControlVariants,
-    ): React.ReactNode => {
-        switch (variant) {
-            case InputVariantTypes.FILLED: {
-                return (
-                    <FilledInput
-                        id={formInputId}
-                        aria-describedby={formHelperTextId}
-                        defaultValue={value}
-                    />
-                );
-            }
-            case InputVariantTypes.OUTLINED: {
-                return (
-                    <OutlinedInput
-                        id={formInputId}
-                        aria-describedby={formHelperTextId}
-                        label={label}
-                        defaultValue={value}
-                    />
-                );
-            }
-            case InputVariantTypes.STANDART: {
-                return (
-                    <Input
-                        id={formInputId}
-                        aria-describedby={formHelperTextId}
-                        defaultValue={value}
-                    />
-                );
-            }
-            default: {
-                return null;
-            }
-        }
-    };
-
-    return (
-        <MUIFormControl
-            variant={variant}
-            color={color}
-            error={isError}
-            disabled={isDisabled}
-        >
-            {label && (
-                <InputLabel htmlFor={formInputId} {...inputLabelProps}>
-                    {label}
-                </InputLabel>
-            )}
-
-            {renderVariantInput(variant)}
-
-            {helperText && (
-                <FormHelperText id={formHelperTextId} {...formHelperTextProps}>
-                    {helperText}
-                </FormHelperText>
-            )}
-        </MUIFormControl>
-    );
-};
+    className = '',
+    sx = [],
+}) => (
+    <MUIFormControl
+        color={color}
+        variant={variant}
+        error={isError}
+        disabled={isDisabled}
+        className={className}
+        sx={sx}
+    >
+        {children}
+    </MUIFormControl>
+);
 
 export { FormControl };
