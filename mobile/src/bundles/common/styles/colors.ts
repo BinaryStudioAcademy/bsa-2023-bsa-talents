df989b24bdf1c36600e0a1d92dc231177e03dd28
dobroillya <dobroillya@gmail.com>
2023-08-21 15:37:44 +0300

    bt-17: * colors

--- mobile/src/bundles/common/styles/colors.ts
+++ mobile/src/bundles/common/styles/colors.ts
@@ -1,15 +1,13 @@
 const colors = {
     primary: '#274F8D',
+    one: '#18A0FB',
+    error: '#FF2121',
     hover: '#0084DD',
     background: '#F7F8FC',
     input: '#D5DCE8',
-    text: '#1F1E29',
     text2: '#90A3BF',
+    textDark: '#7C8798',
     tag: '#DCE5FF',
-    gray: '#AAB1BB',
-    lightGray: '#E0E0E0',
-    white: '#FFFFFF',
-    black: '#000000',
 };
 
 export { colors };
