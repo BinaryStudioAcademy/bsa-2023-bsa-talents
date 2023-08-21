9e4b488b62013c291831c099b042a01994e084f6
dobroillya <dobroillya@gmail.com>
2023-08-21 14:09:15 +0300

    bt-17: + colors.ts

---
+++ mobile/src/bundles/common/theme/colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+    primary: '#274F8D',
+    hover: '#0084DD',
+    background: '#F7F8FC',
+    backgroundDark: '#1F1E29',
+    input: '#D5DCE8',
+    text: '#1F1E29',
+    text2: '#90A3BF',
+    tag: '#DCE5FF',
+    lightGray: '#E0E0E0',
+    white: '#FFFFFF',
+    black: '#000000',
+};
+
+export { colors };
