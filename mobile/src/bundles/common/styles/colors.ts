096865de4a6356846ea86c93265740c8769320ed
dobroillya <dobroillya@gmail.com>
2023-08-21 16:13:19 +0300

    bt-17: * colors

--- mobile/src/bundles/common/styles/colors.ts
+++ mobile/src/bundles/common/styles/colors.ts
@@ -1,13 +1,13 @@
-const colors = {
-    primary: '#274F8D',
-    one: '#18A0FB',
-    error: '#FF2121',
-    hover: '#0084DD',
-    background: '#F7F8FC',
-    input: '#D5DCE8',
-    text2: '#90A3BF',
-    textDark: '#7C8798',
-    tag: '#DCE5FF',
-};
+const Color = {
+    PRIMARY: '#274F8D',
+    SECONDARY: '#18A0FB',
+    ERROR: '#FF2121',
+    HOVER: '#0084DD',
+    BACKGROUND: '#F7F8FC',
+    INPUT: '#D5DCE8',
+    TEXT2: '#90A3BF',
+    TEXTDARK: '#7C8798',
+    TAG: '#DCE5FF',
+} as const;
 
-export { colors };
+export { Color };
