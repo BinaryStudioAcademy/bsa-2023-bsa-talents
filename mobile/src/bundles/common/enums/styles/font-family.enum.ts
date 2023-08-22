71bc3affc797a391a9338bc1d9b369ebd322fbf8
Tatiana Ubozhenko <tatiana.medvedeva.100@gmail.com>
2023-08-22 17:08:36 +0300

    bt-14: * fix file names for eslint

--- mobile/src/bundles/common/enums/styles/font-family.enum.ts
+++ mobile/src/bundles/common/enums/styles/font-family.enum.ts
@@ -1,12 +1,12 @@
 const FontFamily = {
     INTER_100: 'inter-thin',
-    INTER_200: 'inter-extraLight',
+    INTER_200: 'inter-extralight',
     INTER_300: 'inter-light',
     INTER_400: 'inter-regular',
     INTER_500: 'inter-medium',
-    INTER_600: 'inter-semiBold',
+    INTER_600: 'inter-semibold',
     INTER_700: 'inter-bold',
-    INTER_800: 'inter-extraBold',
+    INTER_800: 'inter-extrabold',
     INTER_900: 'inter-black',
 } as const;
 
