12e4d072eb6849f5ac49b65719f06bde4a020db3
mchornomaz <chornomaz.maksym@gmail.com>
2023-08-22 15:04:22 +0300

    bt-5: * fix-select-component-styling

--- frontend/src/bundles/common/components/select/select.tsx
+++ frontend/src/bundles/common/components/select/select.tsx
@@ -1,11 +1,9 @@
 import {
     FormControl,
     FormHelperText,
-    InputLabel,
     MenuItem,
     Select as MuiSelect,
 } from '@mui/material';
-import { alpha, styled } from '@mui/material/styles';
 import {
     type Control,
     type FieldErrors,
@@ -18,63 +16,27 @@ import {
 import { useFormController } from '~/bundles/common/hooks/hooks.js';
 
 import arrowIcon from '../../../../assets/img/select-arrow.svg';
+import styles from './select.module.scss';
 
-type SelectProperties<T extends FieldValues> = {
+type Properties<T extends FieldValues> = {
     control: Control<T, null>;
     errors: FieldErrors<T>;
-    label: string;
     name: FieldPath<T>;
     options: { value: string | number; label: string }[];
     multiple?: boolean;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const SelectContainer = styled(FormControl)(({ theme }) => ({
-    width: '100%',
-    fontFamily: '"Inter", sans-serif',
-}));
-
-const SelectLabel = styled(InputLabel)({
-    fontSize: '13px',
-    fontFamily: '"Inter", sans-serif',
-    fontStyle: 'normal',
-    lineHeight: 'normal',
-    top: '-5px',
-    color: '#1f1e29',
-    fontWeight: 500,
-});
-
-const shadowOpacity = 0.25;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const SelectInput = styled(MuiSelect)(({ theme }) => ({
-    height: '38px',
-    padding: '10px 7px 12px 14px',
-    color: '#90a3bf',
-    fontWeight: 400,
-    transition: 'border-color 0.3s, box-shadow 0.3s',
-    '&:focus': {
-        borderColor: '#80bdff',
-        boxShadow: `0 0 0 0.2rem ${alpha('#007BFF', shadowOpacity)}`,
-    },
-}));
-
-const ArrowIcon = styled('svg')({
-    width: '20px',
-    height: '20px',
-    transform: 'rotate(0deg)',
-    transition: 'transform 0.3s ease',
-    backgroundImage: `url(${arrowIcon})`,
-});
+const ArrowIcon = (): JSX.Element => {
+    return <img className={styles.icon} src={arrowIcon} alt="arrow icon" />;
+};
 
 const Select = <T extends FieldValues>({
     control,
     errors,
-    label,
     name,
     options,
     multiple,
-}: SelectProperties<T>): JSX.Element => {
+}: Properties<T>): JSX.Element => {
     const firstElementIndex = 0;
     const { field } = useFormController({
         name,
@@ -88,21 +50,21 @@ const Select = <T extends FieldValues>({
     const hasError = Boolean(error);
 
     return (
-        <SelectContainer error={hasError}>
-            <SelectLabel>{label}</SelectLabel>
-            <SelectInput
+        <FormControl error={hasError} className={styles.container}>
+            <MuiSelect
                 {...field}
                 multiple={multiple}
                 IconComponent={ArrowIcon}
+                className={styles.input}
             >
                 {options.map((option) => (
                     <MenuItem key={option.value} value={option.value}>
                         {option.label}
                     </MenuItem>
                 ))}
-            </SelectInput>
+            </MuiSelect>
             {hasError && <FormHelperText>{error as string}</FormHelperText>}
-        </SelectContainer>
+        </FormControl>
     );
 };
 
