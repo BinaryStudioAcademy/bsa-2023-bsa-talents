38d52354f54bba103a1421d10b604f33eb1c7af1
vladjsp <vladjsp@gmail.com>
2023-08-22 14:12:22 +0300

    bt-9: + basic input component without adornsment

--- frontend/src/bundles/common/components/input/input.tsx
+++ frontend/src/bundles/common/components/input/input.tsx
@@ -6,7 +6,13 @@ import {
     type FieldValues,
 } from 'react-hook-form';
 
+//import searchIcon from '~/assets/img/search.svg';
 import { useFormController } from '~/bundles/common/hooks/hooks.js';
+
+import { getValidClassNames } from '../../helpers/helpers.js';
+import styles from './styles.module.scss';
+
+type InputType = 'text' | 'email' | 'password' | 'search';
 
 type Properties<T extends FieldValues> = {
     control: Control<T, null>;
@@ -14,7 +20,8 @@ type Properties<T extends FieldValues> = {
     label: string;
     name: FieldPath<T>;
     placeholder?: string;
-    type?: 'text' | 'email';
+    type?: InputType;
+    disabled?: boolean;
 };
 
 const Input = <T extends FieldValues>({
@@ -24,21 +31,34 @@ const Input = <T extends FieldValues>({
     name,
     placeholder = '',
     type = 'text',
+    disabled = false,
 }: Properties<T>): JSX.Element => {
     const { field } = useFormController({ name, control });
 
     const error = errors[name]?.message;
     const hasError = Boolean(error);
 
+    const generatedRootStyles = getValidClassNames(
+        styles.root,
+        disabled && styles.disabled,
+        hasError && styles.error,
+    );
+
     return (
-        <label>
-            <span>{label}</span>
+        <label className={styles.wrapper}>
+            <span className={disabled ? styles.labelDisabled : styles.label}>
+                {label}
+            </span>
             <TextField
                 {...field}
                 type={type}
                 placeholder={placeholder}
                 error={hasError}
-                helperText={error as string}
+                helperText={(error as string) || ' '}
+                disabled={disabled}
+                className={generatedRootStyles}
+                InputProps={{ className: styles['MuiInputBase-root'] }}
+                inputProps={{ className: styles['MuiInputBase-input'] }}
             />
         </label>
     );
