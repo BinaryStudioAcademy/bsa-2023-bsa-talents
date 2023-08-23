fd4c590d95b64ed3a10934bef2c4fa4453c4c078
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-08-23 10:42:30 +0300

    bt-60: + use global styles

--- mobile/src/bundles/common/components/components.tsx
+++ mobile/src/bundles/common/components/components.tsx
@@ -1,4 +1,5 @@
 export { Button } from './button/button';
 export { Input } from './input/input';
 export { Link } from './link/link';
+export { Loader } from './loader/loader';
 export { ActivityIndicator, Text, View } from 'react-native';
--- mobile/src/bundles/common/components/loader/loader.tsx
+++ mobile/src/bundles/common/components/loader/loader.tsx
@@ -5,15 +5,20 @@ import {
     View,
 } from '~/bundles/common/components/components';
 import { Color } from '~/bundles/common/enums/enums';
-
-import { styles } from './styles';
+import { globalStyles } from '~/bundles/common/styles/styles';
 
 type LoaderProperties = {
     size: 'small' | 'large';
 };
 const Loader: React.FC<LoaderProperties> = ({ size }) => {
     return (
-        <View style={styles.wrapper}>
+        <View
+            style={[
+                globalStyles.flex1,
+                globalStyles.alignItemsCenter,
+                globalStyles.justifyContentCenter,
+            ]}
+        >
             <ActivityIndicator color={Color.PRIMARY} size={size} />
         </View>
     );
