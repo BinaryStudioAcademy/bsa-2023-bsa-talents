83a6f7612b3f3aa75dc52875a0145deefcde8618
nika-sirak <7nicka@gmail.com>
2023-08-23 08:03:19 +0100

    bt-7: * code style

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import '~/assets/css/styles.scss';
 
-import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from '@mui/material/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -11,38 +11,35 @@ import {
     StoreProvider,
 } from '~/bundles/common/components/components.js';
 import { AppRoute } from '~/bundles/common/enums/enums.js';
-import { theme } from '~/bundles/common/theme/theme.js';
 import { store } from '~/framework/store/store.js';
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
     <StrictMode>
-        <ThemeProvider theme={theme}>
-            <StyledEngineProvider injectFirst>
-                <StoreProvider store={store.instance}>
-                    <RouterProvider
-                        routes={[
-                            {
-                                path: AppRoute.ROOT,
-                                element: <App />,
-                                children: [
-                                    {
-                                        path: AppRoute.ROOT,
-                                        element: 'Root',
-                                    },
-                                    {
-                                        path: AppRoute.SIGN_IN,
-                                        element: <Auth />,
-                                    },
-                                    {
-                                        path: AppRoute.SIGN_UP,
-                                        element: <Auth />,
-                                    },
-                                ],
-                            },
-                        ]}
-                    />
-                </StoreProvider>
-            </StyledEngineProvider>
-        </ThemeProvider>
+        <StyledEngineProvider injectFirst>
+            <StoreProvider store={store.instance}>
+                <RouterProvider
+                    routes={[
+                        {
+                            path: AppRoute.ROOT,
+                            element: <App />,
+                            children: [
+                                {
+                                    path: AppRoute.ROOT,
+                                    element: 'Root',
+                                },
+                                {
+                                    path: AppRoute.SIGN_IN,
+                                    element: <Auth />,
+                                },
+                                {
+                                    path: AppRoute.SIGN_UP,
+                                    element: <Auth />,
+                                },
+                            ],
+                        },
+                    ]}
+                />
+            </StoreProvider>
+        </StyledEngineProvider>
     </StrictMode>,
 );
