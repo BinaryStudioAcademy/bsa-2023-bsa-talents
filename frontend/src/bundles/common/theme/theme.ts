58643fa2fa2584c8e7c8e8dd55bb9662c93ff05a
nika-sirak <7nicka@gmail.com>
2023-08-23 22:47:00 +0100

    bt-7: + typography enum

--- frontend/src/bundles/common/theme/theme.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-
-import { typographyTheme } from './typography-theme.js';
-
-const theme = createTheme({
-    ...typographyTheme,
-});
-
-export { theme };
