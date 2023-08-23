c690714e8ce243fafb321c7068f28000ea282aa3
mchornomaz <chornomaz.maksym@gmail.com>
2023-08-23 14:19:23 +0300

    bt-66: * changed return logic and added props children

--- frontend/src/bundles/auth/components/protected-route/protected-route.tsx
+++ frontend/src/bundles/auth/components/protected-route/protected-route.tsx
@@ -1,23 +1,31 @@
-import { type FC } from 'react';
-import { type RouteProps } from 'react-router-dom';
-import { Navigate, Route } from 'react-router-dom';
+import { type FC, type ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
 
+import { Loader } from '~/bundles/common/components/loader/loader.js';
 import { AppRoute } from '~/bundles/common/enums/app-route.enum.js';
 import { DataStatus } from '~/bundles/common/enums/data-status.enum.js';
 import { useAppSelector } from '~/bundles/common/hooks/hooks.js';
 
-const ProtectedRoute: FC<RouteProps> = ({ ...props }) => {
+type ProtectedRouteProperties = {
+    children: ReactNode;
+};
+
+const ProtectedRoute: FC<ProtectedRouteProperties> = ({ children }) => {
     const { dataStatus } = useAppSelector(({ users }) => ({
         dataStatus: users.dataStatus,
     }));
 
     const isAuth = dataStatus === DataStatus.FULFILLED;
 
-    if (!isAuth) {
-        return <Navigate to={AppRoute.SIGN_IN} replace />;
+    if (dataStatus === DataStatus.PENDING) {
+        return <Loader />;
     }
 
-    return <Route {...props} />;
+    return isAuth ? (
+        <>{children}</>
+    ) : (
+        <Navigate to={AppRoute.SIGN_IN} replace />
+    );
 };
 
 export { ProtectedRoute };
