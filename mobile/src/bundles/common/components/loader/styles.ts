c5617fe87b4ef48109b72775bffecdd3245da587
Maksimenko Maksim <mskmee2@gmail.com>
2023-08-23 08:36:41 +0300

    bt-60: + fix styles

--- mobile/src/bundles/common/components/loader/styles.ts
+++ mobile/src/bundles/common/components/loader/styles.ts
@@ -2,7 +2,8 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     wrapper: {
-        flex: 1,
+        width: '100%',
+        height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
     },
