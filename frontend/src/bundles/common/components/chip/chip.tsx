9761152e32e39aa0b49b5044bd9c9ddcd2fb7af2
Chichik Anastasiia <chichiknastya@gmail.com>
2023-08-23 14:02:10 +0300

    bt-6: * chip className and deleteIcon prop

--- frontend/src/bundles/common/components/chip/chip.tsx
+++ frontend/src/bundles/common/components/chip/chip.tsx
@@ -1,7 +1,7 @@
 import { Close } from '@mui/icons-material';
 import { Chip as MUIChip } from '@mui/material';
-import clsx from 'clsx';
 
+import { getValidClassNames } from '../../helpers/helpers.js';
 import styles from './styles.module.scss';
 
 type Properties = {
@@ -12,10 +12,12 @@ type Properties = {
 const Chip = ({ label, onDelete }: Properties): JSX.Element => {
     return (
         <MUIChip
-            className={clsx(styles.chip, { [styles['delete-chip']]: onDelete })}
+            className={getValidClassNames(styles.chip, {
+                [styles['delete-chip']]: onDelete,
+            })}
             label={label}
             onDelete={onDelete}
-            deleteIcon={onDelete ? <Close /> : undefined}
+            deleteIcon={onDelete && <Close />}
         />
     );
 };
