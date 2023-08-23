e4eba33e2bf460191c1baccd3d168828ad8430bf
dobroillya <dobroillya@gmail.com>
2023-08-23 13:35:47 +0300

    bt-33: - Modal

--- mobile/src/bundles/common/components/components.tsx
+++ mobile/src/bundles/common/components/components.tsx
@@ -2,10 +2,4 @@ export { Button } from './button/button';
 export { Input } from './input/input';
 export { Link } from './link/link';
 export { Text } from './text/text';
-export {
-    FlatList,
-    Modal,
-    Pressable,
-    TouchableOpacity,
-    View,
-} from 'react-native';
+export { FlatList, Pressable, TouchableOpacity, View } from 'react-native';
