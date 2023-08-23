224fa73cae2aa1edc6cfd51748b2dc2565708d8e
Maksimenko Maksim <mskmee2@gmail.com>
2023-08-23 08:43:55 +0300

    bt-65: + change hover

--- mobile/src/bundles/common/enums/ui/colors.enum.ts
+++ mobile/src/bundles/common/enums/ui/colors.enum.ts
@@ -2,7 +2,7 @@ const Color = {
     PRIMARY: '#274F8D',
     SECONDARY: '#2E61AE',
     ERROR: '#FF2121',
-    HOVER: '#0084DD',
+    HOVER: '#2E61AE',
     BACKGROUND: '#F7F8FC',
     INPUT: '#D5DCE8',
     TEXT2: '#90A3BF',
