22706d8f29b7cd28558503e6e6696fb93c25a7e0
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-08-24 20:24:04 +0300

    bt-78: * props and funcs of modal component

--- frontend/src/bundles/common/components/modal/modal.tsx
+++ frontend/src/bundles/common/components/modal/modal.tsx
@@ -1,6 +1,5 @@
 import { CloseRounded } from '@mui/icons-material';
 import { Divider, Modal as MUIModal } from '@mui/material';
-import { useCallback, useState } from 'react';
 
 import { Button, Typography } from '../components.js';
 import styles from './styles.module.scss';
@@ -8,61 +7,40 @@ import styles from './styles.module.scss';
 type Properties = {
     children: React.ReactNode;
     headerLabel: string;
-    openButtonLabel: string;
     className?: string;
+    isOpen: boolean;
+    onClose: () => void;
 };
 
 const Modal: React.FC<Properties> = ({
     className,
     children,
     headerLabel,
-    openButtonLabel,
-}) => {
-    const [open, setOpen] = useState<boolean>(false);
-
-    const handleOpen = useCallback((): void => {
-        setOpen(true);
-    }, []);
-
-    const handleClose = useCallback((): void => {
-        setOpen(false);
-    }, []);
-
-    return (
-        <div>
-            <Button onClick={handleOpen} label={openButtonLabel} />
-            <MUIModal
-                open={open}
-                onClose={handleClose}
-                className={styles.modalWrapper}
-            >
-                <div className={styles.modalContent}>
-                    <div className={styles.modalHeader}>
-                        <Typography variant="h6">{headerLabel}</Typography>
-                        <div className={styles.closeButtonWrapper}>
-                            <Divider
-                                className={styles.dividerVertical}
-                                orientation="vertical"
-                            />
-                            <Button
-                                onClick={handleClose}
-                                className={styles.iconButton}
-                                label=""
-                                variant="outlined"
-                                endIcon={
-                                    <CloseRounded
-                                        className={styles.closeIcon}
-                                    />
-                                }
-                            />
-                        </div>
-                    </div>
-                    <Divider />
-                    <div className={className}>{children}</div>
+    isOpen,
+    onClose,
+}) => (
+    <MUIModal open={isOpen} onClose={onClose} className={styles.modalWrapper}>
+        <div className={styles.modalContent}>
+            <div className={styles.modalHeader}>
+                <Typography variant="h6">{headerLabel}</Typography>
+                <div className={styles.closeButtonWrapper}>
+                    <Divider
+                        className={styles.dividerVertical}
+                        orientation="vertical"
+                    />
+                    <Button
+                        onClick={onClose}
+                        className={styles.iconButton}
+                        label=""
+                        variant="outlined"
+                        endIcon={<CloseRounded className={styles.closeIcon} />}
+                    />
                 </div>
-            </MUIModal>
+            </div>
+            <Divider />
+            <div className={className}>{children}</div>
         </div>
-    );
-};
+    </MUIModal>
+);
 
 export { Modal };
