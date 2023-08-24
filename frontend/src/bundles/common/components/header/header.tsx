d35a41d09ac1f9e531b5da624866f1e7325186d2
savinaDaria <darianna697@gmail.com>
2023-08-24 11:03:27 +0300

    bt-52: * add  getValidClassNames to combine styles

--- frontend/src/bundles/common/components/header/header.tsx
+++ frontend/src/bundles/common/components/header/header.tsx
@@ -1,3 +1,5 @@
+import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
+
 import styles from './styles.module.scss';
 
 type Properties = {
@@ -6,15 +8,15 @@ type Properties = {
 };
 
 const Header: React.FC<Properties> = ({ avatarUrl, isOnline }) => {
+    const statusCLasses = getValidClassNames(
+        styles.status,
+        isOnline ? styles.online : styles.offline,
+    );
     return (
         <header className={styles.header}>
             <div className={styles.avatar}>
                 <img src={avatarUrl} alt="User Avatar" />
-                <div
-                    className={`${styles.status} ${
-                        isOnline ? styles.online : styles.offline
-                    }`}
-                />
+                <div className={statusCLasses} />
             </div>
         </header>
     );
