19b8de10569ce85e3b5369b7e5718dccbca9c0e6
Matvey V <main.monyster@gmail.com>
2023-08-24 23:35:07 +0300

    bt-86: * update sign-up flow

--- backend/src/bundles/auth/auth.service.ts
+++ backend/src/bundles/auth/auth.service.ts
@@ -3,6 +3,8 @@ import {
     type UserSignUpResponseDto,
 } from '~/bundles/users/types/types.js';
 import { type UserService } from '~/bundles/users/user.service.js';
+// TODO: after task #19 merged (getByEmail need)
+// import { HttpCode, HttpError } from '~/common/http/http.js';
 
 class AuthService {
     private userService: UserService;
@@ -11,10 +13,21 @@ class AuthService {
         this.userService = userService;
     }
 
-    public signUp(
+    public async signUp(
         userRequestDto: UserSignUpRequestDto,
     ): Promise<UserSignUpResponseDto> {
-        return this.userService.create(userRequestDto);
+        // TODO: after task #19 merged (getByEmail need)
+        // const { email } = userRequestDto;
+        // const userByEmail = await this.userService.getByEmail(email);
+        // if (userByEmail) {
+        //     throw new HttpError({
+        //         message: 'User already exist.',
+        //         cause: 'Email already used',
+        //         status: HttpCode.CONFLICT,
+        //     });
+        // }
+
+        return await this.userService.create(userRequestDto);
     }
 }
 
