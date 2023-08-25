87f80e6e47870990439c363cd12db5261be6a51d
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-08-25 18:33:58 +0300

    bt-110: * helper

--- shared/src/helpers/get-avatar-initials/get-avatar-initials.ts
+++ shared/src/helpers/get-avatar-initials/get-avatar-initials.ts
@@ -1,7 +1,7 @@
 const getAvatarInitials = (fullName: string): string => {
     const [name, secondName] = fullName;
     const firstCharIndex = 0;
-    return `${name[firstCharIndex].toUpperCase()} ${secondName[
+    return `${name[firstCharIndex].toUpperCase()}${secondName[
         firstCharIndex
     ].toUpperCase()}`;
 };
