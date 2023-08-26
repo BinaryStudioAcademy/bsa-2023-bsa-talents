bdf5592613aeb013b2f54c69e777c337aecb227c
nika-sirak <7nicka@gmail.com>
2023-08-26 23:19:40 +0100

    bt-72: + bsa badges table migration

--- backend/src/common/database/enums/database-table-name.enum.ts
+++ backend/src/common/database/enums/database-table-name.enum.ts
@@ -2,6 +2,7 @@ const DatabaseTableName = {
     MIGRATIONS: 'migrations',
     USERS: 'users',
     FILES: 'files',
+    BSA_BADGES: 'bsa_badges',
 } as const;
 
 export { DatabaseTableName };
---
+++ backend/src/migrations/20230826220746_add_bsa_badges_table.ts
@@ -0,0 +1,35 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'bsa_badges';
+
+const ColumnName = {
+    ID: 'id',
+    TYPE: 'type',
+    NAME: 'name',
+    MAX_SCORE: 'max_score',
+    CREATED_AT: 'created_at',
+    UPDATED_AT: 'updated_at',
+};
+
+async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable(TABLE_NAME, (table) => {
+        table.increments(ColumnName.ID).primary();
+        table.string(ColumnName.TYPE).unique().notNullable();
+        table.string(ColumnName.NAME).notNullable();
+        table.integer(ColumnName.MAX_SCORE).notNullable();
+        table
+            .dateTime(ColumnName.CREATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        table
+            .dateTime(ColumnName.UPDATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+    });
+}
+
+async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists(TABLE_NAME);
+}
+
+export { down, up };
