a1352d1049bc360e6b35d390ac11b80ed0a2c660
savinaDaria <darianna697@gmail.com>
2023-08-26 13:16:25 +0300

    bt-87: - remove custom preHandler type

--- backend/src/common/controller/types/api-prehandler.type.ts
+++
@@ -1,9 +0,0 @@
-import { type FastifyReply, type FastifyRequest } from 'fastify';
-
-type PreHandlerHook = (
-    request: FastifyRequest,
-    reply: FastifyReply,
-    done: (error?: Error) => void,
-) => void;
-
-export { type PreHandlerHook };
