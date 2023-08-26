93cc2ba6ca9e58962942ae31070f391831d14c03
dobroillya <dobroillya@gmail.com>
2023-08-26 17:57:31 +0300

    bt-108: + buttons

--- mobile/src/bundles/users/screens/bsa-badges.tsx
+++ mobile/src/bundles/users/screens/bsa-badges.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-import { View } from '~/bundles/common/components/components';
+import { Button, View } from '~/bundles/common/components/components';
+import { ButtonType } from '~/bundles/common/enums/enums';
 import { globalStyles } from '~/bundles/common/styles/global-styles';
+
+import { Badge } from '../components/badge/badge';
 
 // import { Badge } from '../components/badge/badge';
 
 const BsaBadges: React.FC = () => {
-    return <View style={[globalStyles.p25]}>{/* <Badge /><Badge /> */}</View>;
+    return (
+        <View style={[globalStyles.p25]}>
+            <Badge />
+            <Badge />
+            <View style={[globalStyles.flexDirectionRow, globalStyles.mt25]}>
+                <Button
+                    style={[globalStyles.mr15, globalStyles.ph25]}
+                    label="Back"
+                    buttonType={ButtonType.OUTLINE}
+                />
+                <Button label="Next" />
+            </View>
+        </View>
+    );
 };
 
 export { BsaBadges };
