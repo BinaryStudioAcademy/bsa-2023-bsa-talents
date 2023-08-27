f825ecf776848aca69c39d49d2bd81e2b897275a
nika-sirak <7nicka@gmail.com>
2023-08-27 11:27:46 +0100

    bt-72: + talent badges table

--- backend/src/common/database/enums/database-table-name.enum.ts
+++ backend/src/common/database/enums/database-table-name.enum.ts
@@ -3,6 +3,7 @@ const DatabaseTableName = {
     USERS: 'users',
     FILES: 'files',
     BSA_BADGES: 'bsa_badges',
+    TALENT_BADGES: 'talent_badges',
 } as const;
 
 export { DatabaseTableName };
---
+++ backend/src/migrations/20230827061743_add_talent_badges_table.ts
@@ -0,0 +1,52 @@
+import { type Knex } from 'knex';
+
+const TableName = {
+    TALENT_BADGES: 'talent_badges',
+    BSA_BADGES: 'bsa_badges',
+};
+
+const ColumnName = {
+    ID: 'id',
+    USER_EMAIL: 'user_email',
+    BADGE_ID: 'badge_id',
+    USER_DETAILS_ID: 'user_details_id',
+    SCORE: 'score',
+    LEVEL: 'level',
+    IS_SHOWN: 'is_shown',
+    CREATED_AT: 'created_at',
+    UPDATED_AT: 'updated_at',
+};
+
+const RelationRule = {
+    CASCADE: 'CASCADE',
+} as const;
+
+async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable(TableName.TALENT_BADGES, (table) => {
+        table.increments(ColumnName.ID).primary();
+        table.integer(ColumnName.SCORE);
+        table.string(ColumnName.LEVEL);
+        table.boolean(ColumnName.IS_SHOWN).notNullable();
+        table.string(ColumnName.USER_EMAIL).unique().notNullable();
+        table
+            .integer(ColumnName.BADGE_ID)
+            .references(ColumnName.ID)
+            .inTable(TableName.BSA_BADGES)
+            .onUpdate(RelationRule.CASCADE)
+            .onDelete(RelationRule.CASCADE);
+        table
+            .dateTime(ColumnName.CREATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        table
+            .dateTime(ColumnName.UPDATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+    });
+}
+
+async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists(TableName.TALENT_BADGES);
+}
+
+export { down, up };
