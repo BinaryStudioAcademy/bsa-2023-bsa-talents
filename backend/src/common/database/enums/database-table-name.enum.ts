e1c61e4bd0395b526cf7e0b4695057ae9e0320c5
nika-sirak <7nicka@gmail.com>
2023-08-27 12:56:53 +0100

    bt-72: + talent badges enum

--- backend/src/common/database/enums/database-table-name.enum.ts
+++ backend/src/common/database/enums/database-table-name.enum.ts
@@ -2,8 +2,8 @@ const DatabaseTableName = {
     MIGRATIONS: 'migrations',
     USERS: 'users',
     FILES: 'files',
-    BSA_BADGES: 'bsa_badges',
     TALENT_BADGES: 'talent_badges',
+    BSA_BADGES: 'bsa_badges',
 } as const;
 
 export { DatabaseTableName };
