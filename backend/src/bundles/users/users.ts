9c3925751adc6678e98f685f8587b07a5b99d25a
Paul <rayver2012@gmail.com>
2023-08-28 16:47:19 +0300

    bt-19: * Lint fixes

--- backend/src/bundles/users/users.ts
+++ backend/src/bundles/users/users.ts
@@ -17,4 +17,7 @@ export {
 } from './types/types.js';
 export { UserModel } from './user.model.js';
 export { UserService } from './user.service.js';
-export { userSignInValidationSchema, userSignUpValidationSchema } from './validation-schemas/validation-schemas.js';
+export {
+    userSignInValidationSchema,
+    userSignUpValidationSchema,
+} from './validation-schemas/validation-schemas.js';
