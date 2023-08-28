0ac3a7a4bb856aff08f70dd4b96c83a775145af5
Paul <rayver2012@gmail.com>
2023-08-28 11:33:34 +0300

    bt-118: * Minor changes

--- backend/src/migrations/20230827100608_remove_password_salt.ts
+++ backend/src/migrations/20230827100608_remove_password_salt.ts
@@ -2,15 +2,19 @@ import { type Knex } from 'knex';
 
 const TABLE_NAME = 'users';
 
+const ColumnName = {
+    PASSWORD_SALT: 'password_salt',
+};
+
 function up(knex: Knex): Promise<void> {
     return knex.schema.table(TABLE_NAME, function (table) {
-        table.dropColumn('password_salt');
+        table.dropColumn(ColumnName.PASSWORD_SALT);
     });
 }
 
 function down(knex: Knex): Promise<void> {
     return knex.schema.table(TABLE_NAME, function (table) {
-        table.string('password_salt');
+        table.string(ColumnName.PASSWORD_SALT);
     });
 }
 
