ded7c9130a526167f3c10acaead9cba9a7d4d0d1
vladjsp <vladjsp@gmail.com>
2023-08-28 13:21:19 +0300

    bt-130: - interface folder from frontend framework api

--- frontend/src/framework/api/types/http-api.type.ts
+++ frontend/src/framework/api/types/http-api.type.ts
@@ -1,4 +1,4 @@
-import { type HttpApiOptions } from '../types/types.js';
+import { type HttpApiOptions } from './types.js';
 
 type HttpApi = {
     load(path: string, options: HttpApiOptions): Promise<Response>;
