55111858d8382671f53aec3a15d6db3202e3ffdb
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-08-28 09:20:19 +0300

    bt-110: * get avatar initials path

--- shared/src/helpers/helpers.ts
+++ shared/src/helpers/helpers.ts
@@ -1,2 +1,2 @@
 export { configureString } from './configure-string/configure-string.helper.js';
-export { getAvatarInitials } from './get-avatar-initials/get-avatar-initials';
+export { getAvatarInitials } from './user/get-avatar-initials';
