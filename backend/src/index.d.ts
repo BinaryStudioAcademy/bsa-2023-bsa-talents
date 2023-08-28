68cd48b3658980d93e7c9150f2ecc14ea18c0fa4
JoshDagat <josuer.online.work@gmail.com>
2023-08-28 19:50:44 +0800

    bt-21: * Add eslint rule

--- backend/src/index.d.ts
+++ backend/src/index.d.ts
@@ -1,7 +1,6 @@
 import { type UserFindResponseDto } from './bundles/users/types/types.js';
 
 declare module 'fastify' {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
     interface FastifyRequest {
         user: UserFindResponseDto;
     }
