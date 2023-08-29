c3a12f46376b81fc4239b2a94e14fb12f2c0a8ac
JoshDagat <josuer.online.work@gmail.com>
2023-08-29 21:50:43 +0800

    bt-115: + Createdd sign-in layout

--- frontend/src/bundles/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/bundles/auth/components/sign-in-form/sign-in-form.tsx
@@ -6,6 +6,7 @@ import {
 import {
     Button,
     Checkbox,
+    FormControl,
     FormLabel,
     Grid,
     Input,
@@ -15,18 +16,19 @@ import {
 import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
 import { useAppForm, useCallback } from '~/bundles/common/hooks/hooks.js';
 
-import { GoogleButton } from '../google-button/google-button.js';
+import { DEFAULT_SIGN_IN_PAYLOAD } from './constants/constants.js';
 import styles from './styles.module.scss';
 
 type Properties = {
+    isTalent: boolean;
     onSubmit: (payload: UserSignInRequestDto) => void;
 };
 
-const SignInForm: React.FC<Properties> = ({ onSubmit }) => {
+const SignInForm: React.FC<Properties> = ({ onSubmit, isTalent = true }) => {
     const { control, errors, handleSubmit } = useAppForm<UserSignInRequestDto>({
         defaultValues: {
-            email: '',
-            password: '',
+            email: DEFAULT_SIGN_IN_PAYLOAD.email,
+            password: DEFAULT_SIGN_IN_PAYLOAD.password,
         },
         validationSchema: userSignInValidationSchema,
     });
@@ -38,80 +40,94 @@ const SignInForm: React.FC<Properties> = ({ onSubmit }) => {
         [handleSubmit, onSubmit],
     );
 
+    const sellingPointText = isTalent
+        ? 'Start your career easily'
+        : 'Find the top talent for your business';
+
     return (
         <>
             <Grid container className={styles.container}>
-                <Grid item xs={6}>
-                    Test1
+                <Grid item xs={12} md={6}>
+                    <Grid item className={styles['selling-point']}>
+                        <div className={styles.logo}></div>
+                        <h1 className={styles.text}>{sellingPointText}</h1>
+                    </Grid>
                 </Grid>
-                <Grid className={styles['form-wrapper']} item xs={6}>
-                    <form onSubmit={handleFormSubmit} className={styles.form}>
-                        <p className={getValidClassNames(styles.header)}>
-                            Hi! Login to your Account
-                        </p>
-                        <GoogleButton />
-                        <a href="/">
-                            <span className={styles['email-sign']}>
-                                or Sign in with Email
-                            </span>
-                        </a>
+                <Grid className={styles['form-wrapper']} item xs={12} md={6}>
+                    <Grid item className={styles.wrapper}>
+                        <form
+                            onSubmit={handleFormSubmit}
+                            className={styles.form}
+                        >
+                            <p className={getValidClassNames(styles.header)}>
+                                Hi! Login to your Account
+                            </p>
 
-                        <div
-                            className={getValidClassNames(
-                                styles['input-container'],
-                                styles.email,
-                            )}
-                        >
-                            <FormLabel className={styles.label}>
-                                Email
-                            </FormLabel>
-                            <Input
-                                control={control}
-                                errors={errors}
-                                placeholder="user@email.com"
-                                name="email"
-                                className={styles.input}
+                            <FormControl
+                                className={getValidClassNames(
+                                    styles['input-container'],
+                                    styles.email,
+                                )}
+                            >
+                                <FormLabel className={styles.label}>
+                                    Email
+                                </FormLabel>
+                                <Input
+                                    control={control}
+                                    errors={errors}
+                                    placeholder="user@email.com"
+                                    name="email"
+                                    className={styles.input}
+                                />
+                            </FormControl>
+                            <FormControl
+                                className={getValidClassNames(
+                                    styles['input-container'],
+                                    styles.password,
+                                )}
+                            >
+                                <FormLabel className={styles.label}>
+                                    Password
+                                </FormLabel>
+                                <Input
+                                    control={control}
+                                    errors={errors}
+                                    type="password"
+                                    placeholder="****"
+                                    name="password"
+                                    className={styles.input}
+                                />
+                            </FormControl>
+                            <Grid item className={styles['auth-options']}>
+                                <Checkbox
+                                    label={
+                                        <Typography variant="label">
+                                            Remember Me?
+                                        </Typography>
+                                    }
+                                    className={getValidClassNames(
+                                        styles.checkbox,
+                                    )}
+                                />
+                                {/* TODO: Link to reset password route */}
+                                <Link to="/" className={styles.forgot}>
+                                    <span>Forgot Password?</span>
+                                </Link>
+                            </Grid>
+                            <Button
+                                label="Login"
+                                className={styles['btn-login']}
                             />
-                        </div>
-                        <div
-                            className={getValidClassNames(
-                                styles['input-container'],
-                                styles.password,
-                            )}
-                        >
-                            <FormLabel className={styles.label}>
-                                Password
-                            </FormLabel>
-                            <Input
-                                control={control}
-                                errors={errors}
-                                type="password"
-                                placeholder="****"
-                                name="password"
-                                className={styles.input}
-                            />
-                        </div>
-                        <div className={styles['auth-options']}>
-                            <Checkbox
-                                label={
-                                    <Typography variant="label">
-                                        Remeber Me?
-                                    </Typography>
-                                }
-                                className={getValidClassNames(styles.checkbox)}
-                            />
-                            <a href="/" className={styles.forgot}>
-                                <span>Forgot Password?</span>
-                            </a>
-                        </div>
-                        <Button label="Login" className={styles['btn-login']} />
-                    </form>
-                    <div className={styles.footer}>
-                        <span className={styles.span}>Not registered Yet?</span>
-                        <Link className={styles.cta} to={'/sign-up'}>
-                            Create an account
-                        </Link>
-                    </div>
+                        </form>
+                        <Grid item className={styles.footer}>
+                            <span className={styles.span}>
+                                Not registered Yet?
+                            </span>
+                            <Link className={styles.cta} to={'/sign-up'}>
+                                Create an account
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </>
