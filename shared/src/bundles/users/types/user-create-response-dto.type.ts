de85644ee3ea145bf82df795c795aded80ddbc56
Matvey V <main.monyster@gmail.com>
2023-08-29 18:27:08 +0300

    bt-86: * minor fix

--- shared/src/bundles/users/types/user-create-response-dto.type.ts
+++ shared/src/bundles/users/types/user-create-response-dto.type.ts
@@ -3,7 +3,7 @@ import { type ValueOf } from '~/types/value-of.type.js';
 import { type UserRole } from '../enums/enums.js';
 
 type UserCreateResponseDto = {
-    id: number;
+    id: string;
     email: string;
     role: ValueOf<typeof UserRole>;
 };
