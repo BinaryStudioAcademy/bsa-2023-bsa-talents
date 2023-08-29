6d99b74af0e8d9085f0db6b301157d11ae752d44
Matvii <42421305+Monyster@users.noreply.github.com>
2023-08-29 11:58:13 +0000

    bt-86: + user role created

--- backend/src/bundles/users/user.model.ts
+++ backend/src/bundles/users/user.model.ts
@@ -2,9 +2,13 @@ import {
     AbstractModel,
     DatabaseTableName,
 } from '~/common/database/database.js';
+import { type UserRole } from '~/common/enums/enums.js';
+import { type ValueOf } from '~/common/types/types.js';
 
 class UserModel extends AbstractModel {
     public 'email': string;
+
+    public 'role': ValueOf<typeof UserRole>;
 
     public 'passwordHash': string;
 
--- shared/src/bundles/users/enums/enums.ts
+++ shared/src/bundles/users/enums/enums.ts
@@ -1,2 +1,3 @@
+export { UserRole } from './user-role.enum.js';
 export { UserValidationMessage } from './user-validation-message.enum.js';
 export { UsersApiPath } from './users-api-path.enum.js';
