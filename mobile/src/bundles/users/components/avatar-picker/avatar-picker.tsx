5f66f693ae849e7b5b872b65459d223dd97d601b
Maksimenko Maksim <mskmee2@gmail.com>
2023-08-29 10:57:58 +0300

    bt-111: + avatar picker

--- mobile/src/bundles/users/components/avatar-picker/avatar-picker.tsx
+++ mobile/src/bundles/users/components/avatar-picker/avatar-picker.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { type StyleProp, type ViewStyle } from 'react-native';
 
-import { Text, View } from '~/bundles/common/components/components';
+import { Avatar, Text } from '~/bundles/common/components/components';
 import { TextCategory } from '~/bundles/common/enums/enums';
-import { globalStyles } from '~/bundles/common/styles/styles';
+import { type AvatarProperty } from '~/bundles/common/types/types';
 import { ImagePicker } from '~/bundles/users/components/image-picker/image-picker';
 
-const AvatarPicker: React.FC = () => {
+type AvatarPickerProperties = {
+    buttonStyle: StyleProp<ViewStyle>;
+} & AvatarProperty;
+const AvatarPicker: React.FC<AvatarPickerProperties> = ({
+    buttonStyle,
+    ...props
+}) => {
     return (
-        <View>
-            {/* TODO: Add Avatar */}
+        <>
+            <Avatar {...props} />
             <Text category={TextCategory.H6}>Upload a new photo</Text>
-            <ImagePicker
-                label="Choose photo"
-                containerStyle={[globalStyles.m25]}
-            />
-        </View>
+            <ImagePicker label="Choose photo" containerStyle={buttonStyle} />
+        </>
     );
 };
 
