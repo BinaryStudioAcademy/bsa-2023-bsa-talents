6b58f77f4194324e9e016c5c79adefa733a10ee7
JemiJe <jeremiah7480@gmail.com>
2023-08-29 11:30:22 +0300

    bt-51: - initialState

--- frontend/src/app/store/slice.ts
+++ frontend/src/app/store/slice.ts
@@ -1,15 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { DataStatus } from '~/bundles/common/enums/enums.js';
-import { type ValueOf } from '~/bundles/common/types/types.js';
-
-type State = {
-    dataStatus: ValueOf<typeof DataStatus>;
-};
-
-const initialState: State = {
-    dataStatus: DataStatus.IDLE,
-};
+const initialState = {};
 
 const { reducer, actions, name } = createSlice({
     initialState,
