f54adbb380e03c25ecbddd7b9b17495739fea6ff
savinaDaria <darianna697@gmail.com>
2023-08-29 13:33:36 +0300

    bt-10: * delete changes from index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,7 @@
 import '~/assets/css/styles.scss';
 
+import { ThemeProvider } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -12,31 +14,37 @@ import {
 import { AppRoute } from '~/bundles/common/enums/enums.js';
 import { store } from '~/framework/store/store.js';
 
+import { theme } from './bundles/common/themes/theme.js';
+
 createRoot(document.querySelector('#root') as HTMLElement).render(
     <StrictMode>
-        <StoreProvider store={store.instance}>
-            <RouterProvider
-                routes={[
-                    {
-                        path: AppRoute.ROOT,
-                        element: <App />,
-                        children: [
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <StoreProvider store={store.instance}>
+                    <RouterProvider
+                        routes={[
                             {
                                 path: AppRoute.ROOT,
-                                element: 'Root',
+                                element: <App />,
+                                children: [
+                                    {
+                                        path: AppRoute.ROOT,
+                                        element: 'Root',
+                                    },
+                                    {
+                                        path: AppRoute.SIGN_IN,
+                                        element: <Auth />,
+                                    },
+                                    {
+                                        path: AppRoute.SIGN_UP,
+                                        element: <Auth />,
+                                    },
+                                ],
                             },
-                            {
-                                path: AppRoute.SIGN_IN,
-                                element: <Auth />,
-                            },
-                            {
-                                path: AppRoute.SIGN_UP,
-                                element: <Auth />,
-                            },
-                        ],
-                    },
-                ]}
-            />
-        </StoreProvider>
+                        ]}
+                    />
+                </StoreProvider>
+            </ThemeProvider>
+        </StyledEngineProvider>
     </StrictMode>,
 );
