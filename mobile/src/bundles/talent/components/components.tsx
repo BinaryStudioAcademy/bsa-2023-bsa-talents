c5268a4f1e678db62b17ff6c6ba15f26ee8f1bba
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-08-30 17:07:51 +0300

    bt-111: + refactor path

--- mobile/src/bundles/talent/components/components.tsx
+++ mobile/src/bundles/talent/components/components.tsx
@@ -1,4 +1,3 @@
 export { AvatarPicker } from './avatar-picker/avatar-picker';
-export { ImagePicker } from './image-picker/image-picker';
 export { NewAccountHeader } from './new-account-header/new-account-header';
 export { ProfileForm } from './profile-form/profile-form';
