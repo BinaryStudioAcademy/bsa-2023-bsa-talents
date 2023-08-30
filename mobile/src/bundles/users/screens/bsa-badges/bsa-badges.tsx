5322e8623643c8aefaff6ad5ebf07ff445f3b918
dobroillya <dobroillya@gmail.com>
2023-08-30 08:05:30 +0300

    bt-108: + header * styles in bsa badges

--- mobile/src/bundles/users/screens/bsa-badges/bsa-badges.tsx
+++ mobile/src/bundles/users/screens/bsa-badges/bsa-badges.tsx
@@ -8,14 +8,24 @@ import {
     Text,
     View,
 } from '~/bundles/common/components/components';
-import { ButtonType, TextCategory } from '~/bundles/common/enums/enums';
-import { useAppForm, useCallback } from '~/bundles/common/hooks/hooks';
+import { type OnboardingScreenName } from '~/bundles/common/enums/enums';
+import {
+    ButtonType,
+    OnboardingScreenNumber,
+    TextCategory,
+} from '~/bundles/common/enums/enums';
+import {
+    useAppForm,
+    useAppRoute,
+    useCallback,
+} from '~/bundles/common/hooks/hooks';
 import { globalStyles } from '~/bundles/common/styles/global-styles';
+import { type ValueOf } from '~/bundles/common/types/types';
 import {
     DEFAULT_VALUE_IS_CHECKED,
     DEFAULT_VALUE_IS_DISABLED,
 } from '~/bundles/users/components/badge/constants/constants';
-import { Badge } from '~/bundles/users/components/components';
+import { Badge, NewAccountHeader } from '~/bundles/users/components/components';
 import { BadgeType } from '~/bundles/users/enums/enums';
 
 import { styles } from './styles';
@@ -26,6 +36,9 @@ const BsaBadges: React.FC = () => {
     const { control } = useAppForm({
         defaultValues: DEFAULT_VALUE_IS_CHECKED,
     });
+    const { name } = useAppRoute();
+    const stepTitle = name as ValueOf<typeof OnboardingScreenName>;
+    const stepNumber = OnboardingScreenNumber[stepTitle];
 
     const handleFormSubmit = useCallback(() => {
         // TODO: add submit logic
@@ -40,7 +53,6 @@ const BsaBadges: React.FC = () => {
                 globalStyles.alignItemsCenter,
             ]}
         >
-            {/* this component in another task */}
             <Checkbox
                 control={control}
                 name={badge}
@@ -51,26 +63,27 @@ const BsaBadges: React.FC = () => {
     ));
 
     return (
-        <View style={globalStyles.defaultScreenPadding}>
-            <View style={globalStyles.mb25}>
-                {/* TODO: add header */}
-                <Text>Some header</Text>
-            </View>
-            <Text
-                category={TextCategory.BODY1}
-                style={[globalStyles.mb25, styles.description]}
-            >
-                Choose BSA badges you want to show in your profile
-            </Text>
-            <ScrollView>{renderBadges}</ScrollView>
-            <View style={[globalStyles.flexDirectionRow, globalStyles.mt25]}>
-                <Button
-                    style={globalStyles.mr10}
-                    label="Back"
-                    buttonType={ButtonType.OUTLINE}
-                />
-                <Button label="Next" onPress={handleFormSubmit} />
-            </View>
+        <View>
+            <NewAccountHeader title={stepTitle} currentStep={stepNumber} />
+            <ScrollView contentContainerStyle={globalStyles.ph25}>
+                <Text
+                    category={TextCategory.BODY1}
+                    style={[globalStyles.pv15, styles.description]}
+                >
+                    Choose BSA badges you want to show in your profile
+                </Text>
+                <View>{renderBadges}</View>
+                <View
+                    style={[globalStyles.flexDirectionRow, globalStyles.mt20]}
+                >
+                    <Button
+                        style={globalStyles.mr10}
+                        label="Back"
+                        buttonType={ButtonType.OUTLINE}
+                    />
+                    <Button label="Next" onPress={handleFormSubmit} />
+                </View>
+            </ScrollView>
         </View>
     );
 };
