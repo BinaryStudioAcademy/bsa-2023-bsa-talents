cb135f8fd892305baa856b981753ee6cd85a3413
vladjsp <vladjsp@gmail.com>
2023-08-31 00:30:39 +0300

    bt-126: + chat header

---
+++ frontend/src/bundles/common/components/chat-header/chat-header.tsx
@@ -0,0 +1,34 @@
+import { Grid } from '~/bundles/common/components/components.js';
+import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+    avatarUrl: string;
+    isOnline: boolean;
+    title: string;
+};
+
+const ChatHeader: React.FC<Properties> = ({ avatarUrl, isOnline, title }) => {
+    const onlineIconClasses = getValidClassNames(
+        styles.icon,
+        isOnline ? styles.online : styles.offline,
+    );
+
+    return (
+        <Grid className={styles.wrapper}>
+            <Grid className={styles.logo}>
+                <img src={avatarUrl} alt="logo" />
+            </Grid>
+            <Grid className={styles.info}>
+                <p className={styles.title}>{title}</p>
+                <Grid className={styles.status}>
+                    <Grid className={onlineIconClasses} />
+                    <p>{isOnline ? 'Online' : 'Offline'}</p>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+export { ChatHeader };
