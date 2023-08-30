70e51f764b28c48f6716246889b5e6c1c9ffab29
Matvey V <main.monyster@gmail.com>
2023-08-30 21:01:15 +0300

    bt-182: * getValidClassNames and className prop

--- frontend/src/bundles/common/components/logo/logo.tsx
+++ frontend/src/bundles/common/components/logo/logo.tsx
@@ -1,22 +1,25 @@
-import clsx from 'clsx';
-
 import logoSvg from '~/assets/img/logo/logo.svg';
 import logoLabelSvg from '~/assets/img/logo/logo-label.svg';
 
+import { getValidClassNames } from '../../helpers/helpers.js';
 import { Grid } from '../components.js';
 import styles from './styles.module.scss';
 
 type Properties = {
+    className?: string;
     isCollapsed?: boolean;
 };
 
-const Logo = ({ isCollapsed = false }: Properties): JSX.Element => {
+const Logo = ({
+    className = '',
+    isCollapsed = false,
+}: Properties): JSX.Element => {
     return (
-        <Grid container className={styles.logo}>
+        <Grid container className={getValidClassNames(styles.logo, className)}>
             <Grid
                 item
                 alignItems="center"
-                className={clsx(
+                className={getValidClassNames(
                     styles.logoIcon,
                     isCollapsed && styles.collapsed,
                 )}
@@ -25,7 +28,11 @@ const Logo = ({ isCollapsed = false }: Properties): JSX.Element => {
             </Grid>
 
             {!isCollapsed && (
-                <Grid item alignItems="center" className={styles.logoLabel}>
+                <Grid
+                    item
+                    alignItems="center"
+                    className={getValidClassNames(styles.logoLabel)}
+                >
                     <img src={logoLabelSvg} alt="Label-BSATalents" />
                 </Grid>
             )}
