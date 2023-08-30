72c6614a5e16a0227a02c147762bf16a579dac3f
dobroillya <dobroillya@gmail.com>
2023-08-30 14:18:46 +0300

    bt-108: * change import path

--- mobile/src/bundles/talent/components/components.tsx
+++ mobile/src/bundles/talent/components/components.tsx
@@ -1,3 +1,3 @@
+export { Badge } from './badge/badge';
 export { NewAccountHeader } from './new-account-header/new-account-header';
 export { ProfileForm } from './profile-form/profile-form';
-export { Badge } from '~/bundles/talent/components/badge/badge';
