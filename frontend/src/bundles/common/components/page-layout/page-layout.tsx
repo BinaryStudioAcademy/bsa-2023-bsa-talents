89b9de32c558677bf01757f5ce788ec53b4225cd
Matvii <42421305+Monyster@users.noreply.github.com>
2023-08-31 13:27:12 +0000

    bt-189: * changed divs to Grid

--- frontend/src/bundles/common/components/page-layout/page-layout.tsx
+++ frontend/src/bundles/common/components/page-layout/page-layout.tsx
@@ -1,4 +1,4 @@
-import { Header, Sidebar } from '../../components/components.js';
+import { Grid, Header, Sidebar } from '../../components/components.js';
 import { getValidClassNames } from '../../helpers/helpers.js';
 import styles from './styles.module.scss';
 
@@ -13,15 +13,15 @@ const PageLayout: React.FC<Properties> = ({
     isOnline,
     children,
 }) => (
-    <div className={getValidClassNames(styles.pageContainer)}>
+    <Grid container className={getValidClassNames(styles.pageContainer)}>
         <Sidebar />
-        <div className={getValidClassNames(styles.mainContainer)}>
+        <Grid item className={getValidClassNames(styles.mainContainer)}>
             <Header avatarUrl={avatarUrl} isOnline={isOnline} />
-            <div className={getValidClassNames(styles.mainContent)}>
+            <Grid item className={getValidClassNames(styles.mainContent)}>
                 {children}
-            </div>
-        </div>
-    </div>
+            </Grid>
+        </Grid>
+    </Grid>
 );
 
 export { PageLayout };
--- frontend/src/bundles/common/pages/not-found/not-found.tsx
+++ frontend/src/bundles/common/pages/not-found/not-found.tsx
@@ -1,37 +1,46 @@
-import { Link, Logo, Typography } from '../../components/components.js';
+import { Grid, Link, Logo, Typography } from '../../components/components.js';
 import { AppRoute } from '../../enums/app-route.enum.js';
 import { getValidClassNames } from '../../helpers/helpers.js';
 import styles from './styles.module.scss';
 
-const NotFoundPage: React.FC = () => (
-    <div className={getValidClassNames(styles.pageContainer)}>
-        <div className={getValidClassNames(styles.header)}>
-            <Logo />
-        </div>
-        <div className={getValidClassNames(styles.text)}>
-            <Typography
-                variant="body1"
-                className={getValidClassNames(styles.description)}
-            >
-                The page you are looking for can’t be found.
-            </Typography>
-            <Typography
-                variant="h1"
-                className={getValidClassNames(styles.code)}
-            >
-                404
-            </Typography>
-        </div>
-        <div className={getValidClassNames(styles.link)}>
-            <Link to={AppRoute.ROOT}>Return to Main</Link>
-        </div>
-        <div className={getValidClassNames(styles.cube)}></div>
-        <div className={getValidClassNames(styles.cube)}></div>
-        <div className={getValidClassNames(styles.cube)}></div>
-        <div className={getValidClassNames(styles.cube)}></div>
-        <div className={getValidClassNames(styles.cube)}></div>
-        <div className={getValidClassNames(styles.cube)}></div>
-    </div>
-);
+const NotFoundPage: React.FC = () => {
+    return (
+        <Grid
+            container
+            xs={12}
+            className={getValidClassNames(styles.pageContainer)}
+        >
+            <Grid item className={getValidClassNames(styles.header)}>
+                <Logo />
+            </Grid>
+
+            <Grid item xs={12} className={getValidClassNames(styles.text)}>
+                <Typography
+                    variant="body1"
+                    className={getValidClassNames(styles.description)}
+                >
+                    The page you are looking for can’t be found.
+                </Typography>
+                <Typography
+                    variant="h1"
+                    className={getValidClassNames(styles.code)}
+                >
+                    404
+                </Typography>
+            </Grid>
+
+            <Grid item xs={12} className={getValidClassNames(styles.link)}>
+                <Link to={AppRoute.ROOT}>Return to Main</Link>
+            </Grid>
+
+            <div className={getValidClassNames(styles.cube)}></div>
+            <div className={getValidClassNames(styles.cube)}></div>
+            <div className={getValidClassNames(styles.cube)}></div>
+            <div className={getValidClassNames(styles.cube)}></div>
+            <div className={getValidClassNames(styles.cube)}></div>
+            <div className={getValidClassNames(styles.cube)}></div>
+        </Grid>
+    );
+};
 
 export { NotFoundPage };
