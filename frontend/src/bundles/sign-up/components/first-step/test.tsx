d20ca72bae3104d91837b488f5e9c133e5285a65
savinaDaria <darianna697@gmail.com>
2023-08-31 17:50:17 +0300

    bt-132: + set proper test layout, remove lately

--- frontend/src/bundles/sign-up/components/first-step/test.tsx
+++ frontend/src/bundles/sign-up/components/first-step/test.tsx
@@ -1,15 +1,27 @@
-import { useAppDispatch, useCallback } from '~/bundles/common/hooks/hooks.js';
+import { Button, Grid } from '~/bundles/common/components/components.js';
+import {
+    useAppDispatch,
+    useAppForm,
+    useCallback,
+} from '~/bundles/common/hooks/hooks.js';
 import { actions as signUpActions } from '~/bundles/sign-up/store/sign-up.js';
 import { type UserSignUpStep1Dto } from '~/bundles/sign-up/types/types.js';
+import { signUpStep1ValidationSchema } from '~/bundles/sign-up/validation-schemas/validation-schemas.js';
 
+import { DEFAULT_SIGN_UP_PAYLOAD_STEP1 } from './constants/constants.js';
 import { FirstStep } from './first-step.js';
 
 type Properties = {
     test?: boolean;
 };
 
-const TestStep1: React.FC<Properties> = () => {
+const TestStepLayout: React.FC<Properties> = () => {
     const dispatch = useAppDispatch();
+
+    const method = useAppForm({
+        defaultValues: DEFAULT_SIGN_UP_PAYLOAD_STEP1,
+        validationSchema: signUpStep1ValidationSchema,
+    });
 
     const handleSignUpSubmit = useCallback(
         (payload: UserSignUpStep1Dto): void => {
@@ -17,7 +29,25 @@ const TestStep1: React.FC<Properties> = () => {
         },
         [dispatch],
     );
-    return <FirstStep onSubmit={handleSignUpSubmit} />;
+
+    const handleValidateBeforeSubmit = useCallback((): void => {
+        void method.handleSubmit(handleSignUpSubmit)();
+    }, [handleSignUpSubmit, method]);
+
+    return (
+        <div>
+            <FirstStep methods={method} />
+            <Grid>
+                <Button label="Back" variant="contained" disabled />
+                <Button
+                    label="Next"
+                    variant="contained"
+                    type="submit"
+                    onClick={handleValidateBeforeSubmit}
+                />
+            </Grid>
+        </div>
+    );
 };
 
-export { TestStep1 };
+export { TestStepLayout };
