fd2a7f53d24afd9f04c5954b0809bb9124bba699
Paul <rayver2012@gmail.com>
2023-08-31 18:25:08 +0300

    bt-124: + Add common avatar component

---
+++ frontend/src/bundles/common/components/avatar/avatar.tsx
@@ -0,0 +1,33 @@
+import { Typography } from '~/bundles/common/components/components.js';
+import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+    userFullName: string;
+    url?: string;
+    size?: 'small' | 'medium';
+};
+
+const Avatar: React.FC<Properties> = ({
+    url,
+    size = 'small',
+    userFullName,
+}) => {
+    const avatarClasses = getValidClassNames(styles.avatar, styles[size]);
+
+    const [userName, userSurname] = userFullName.split(' ');
+    const firstLetterIndex = 0;
+    const userNameAbbreviation = `${userName[
+        firstLetterIndex
+    ].toUpperCase()}${userSurname[firstLetterIndex].toUpperCase()}`;
+    return url ? (
+        <img className={avatarClasses} src={url} alt="User avatar" />
+    ) : (
+        <Typography variant="caption" className={avatarClasses}>
+            {userNameAbbreviation}
+        </Typography>
+    );
+};
+
+export { Avatar };
