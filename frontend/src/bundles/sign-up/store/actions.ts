c76aff3a3e2ce278470da66884478272508193bb
savinaDaria <darianna697@gmail.com>
2023-08-31 17:49:20 +0300

    bt-132: * add experience years mapping in store

--- frontend/src/bundles/sign-up/store/actions.ts
+++ frontend/src/bundles/sign-up/store/actions.ts
@@ -3,6 +3,10 @@ import { type UserSignUpStep1Dto } from 'shared/build/index.js';
 
 import { type AsyncThunkConfig } from '~/bundles/common/types/types.js';
 
+import {
+    DEFAULT_SIGN_UP_PAYLOAD_STEP1,
+    experienceYearsScaled,
+} from '../components/first-step/constants/constants.js';
 import { name as sliceName } from './slice.js';
 
 const signUpStep1 = createAsyncThunk<
@@ -10,6 +14,13 @@ const signUpStep1 = createAsyncThunk<
     UserSignUpStep1Dto,
     AsyncThunkConfig
 >(`${sliceName}/step1`, (registerPayload) => {
+    const exactExperienceValue = experienceYearsScaled.find(
+        (experience) =>
+            experience.scaledValue === registerPayload.experienceYears,
+    );
+    registerPayload.experienceYears = exactExperienceValue
+        ? exactExperienceValue.value
+        : DEFAULT_SIGN_UP_PAYLOAD_STEP1.experienceYears;
     return registerPayload;
 });
 
