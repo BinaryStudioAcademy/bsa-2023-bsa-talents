1e05c75e5c375484bfc9465c45fbc4d2b68a54eb
Paul <rayver2012@gmail.com>
2023-08-31 14:25:30 +0300

    bt-124: + Chat message item/list, chat avatar

---
+++ frontend/src/bundles/chat/components/components.ts
@@ -0,0 +1,3 @@
+export { Avatar } from './avatar/avatar.js';
+export { MessageItem } from './message-item/message-item.js';
+export { MessageList } from './message-list/message-list.js';
---
+++ frontend/src/bundles/chat/components/message-item/message-item.tsx
@@ -0,0 +1,36 @@
+import { Grid, Typography } from '~/bundles/common/components/components.js';
+import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
+
+import { Avatar } from '../components.js';
+import styles from './styles.module.scss';
+
+type Properties = {
+    avatarUrl?: string;
+    userId: string;
+    children: string;
+    userFullName: string;
+};
+
+const MessageItem: React.FC<Properties> = ({
+    avatarUrl,
+    userId,
+    children,
+    userFullName,
+}) => {
+    const currentUserId = 'id'; // TODO: get current user from store
+    const wrapperClasses = getValidClassNames(styles['message-wrapper']);
+    const messageClasses = getValidClassNames(
+        styles.message,
+        currentUserId === userId && styles['message-own'],
+    );
+    return (
+        <Grid item className={wrapperClasses}>
+            <Avatar url={avatarUrl} userFullName={userFullName} />
+            <Typography className={messageClasses} variant="body1">
+                {children}
+            </Typography>
+        </Grid>
+    );
+};
+
+export { MessageItem };
