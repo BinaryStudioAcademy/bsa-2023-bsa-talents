20d16541c56f9a2503bba9e70984e0d1e6ff7c1c
Matvey V <main.monyster@gmail.com>
2023-09-02 21:12:03 +0300

    bt-172: * fix id and types

--- shared/src/bundles/user-details/validation-schemas/user-details-approve.validation-schema.ts
+++ shared/src/bundles/user-details/validation-schemas/user-details-approve.validation-schema.ts
@@ -3,7 +3,7 @@ import joi from 'joi';
 import { type UserDetailsApproveRequestDto } from '../types/types.js';
 
 const userDetailsApprove = joi.object<UserDetailsApproveRequestDto, true>({
-    userId: joi.string().trim().required(),
+    id: joi.string().trim().required(),
     isApproved: joi.boolean().required(),
     deniedReason: joi.string(),
 });
--- shared/src/bundles/user-details/validation-schemas/user-details-update.validation-schema.ts
+++ shared/src/bundles/user-details/validation-schemas/user-details-update.validation-schema.ts
@@ -10,62 +10,65 @@ import {
 } from '../enums/enums.js';
 import { type UserDetailsUpdateRequestDto } from '../types/types.js';
 
-const userDetailsUpdate = joi.object<UserDetailsUpdateRequestDto, true>({
-    userId: joi.string().trim().required(),
+const userDetailsUpdate = joi
+    .object<UserDetailsUpdateRequestDto, true>({
+        id: joi.string().trim(),
+        userId: joi.string().trim(),
 
-    isHired: joi.boolean(),
+        isHired: joi.boolean(),
 
-    profileName: joi.string().trim().allow(null),
+        profileName: joi.string().trim().allow(null),
 
-    salaryExpectation: joi.number(),
-    hiredSalary: joi.number(),
+        salaryExpectation: joi.number(),
+        hiredSalary: joi.number(),
 
-    jobTitle: joi
-        .string()
-        .trim()
-        .valid(...Object.values(JobTitle)),
-    location: joi
-        .string()
-        .trim()
-        .valid(...Object.values(CountryList)),
-
-    experienceYears: joi.number(),
-    employmentType: joi.array().items(
-        joi
+        jobTitle: joi
             .string()
             .trim()
-            .valid(...Object.values(EmploymentType)),
-    ),
-
-    description: joi.string(),
-
-    englishLevel: joi
-        .string()
-        .trim()
-        .valid(...Object.values(EnglishLevel)),
-    notConsidered: joi.array().items(
-        joi
+            .valid(...Object.values(JobTitle)),
+        location: joi
             .string()
             .trim()
-            .valid(...Object.values(NotConsidered)),
-    ),
-    preferredLanguages: joi.array().items(
-        joi
+            .valid(...Object.values(CountryList)),
+
+        experienceYears: joi.number(),
+        employmentType: joi.array().items(
+            joi
+                .string()
+                .trim()
+                .valid(...Object.values(EmploymentType)),
+        ),
+
+        description: joi.string(),
+
+        englishLevel: joi
             .string()
             .trim()
-            .valid(...Object.values(PreferredLanguages)),
-    ),
+            .valid(...Object.values(EnglishLevel)),
+        notConsidered: joi.array().items(
+            joi
+                .string()
+                .trim()
+                .valid(...Object.values(NotConsidered)),
+        ),
+        preferredLanguages: joi.array().items(
+            joi
+                .string()
+                .trim()
+                .valid(...Object.values(PreferredLanguages)),
+        ),
 
-    projectLinks: joi.array().items(joi.string().trim().uri()),
-    photoId: joi.string().trim(),
-    fullName: joi.string().trim(),
-    phone: joi.string().trim(),
-    linkedinLink: joi.string().trim().uri(),
-    companyName: joi.string().trim(),
-    companyLogoId: joi.string().trim(),
-    companyWebsite: joi.string().trim().uri(),
-    employerPosition: joi.string().trim(),
-    cvId: joi.string().trim(),
-});
+        projectLinks: joi.array().items(joi.string().trim().uri()),
+        photoId: joi.string().trim(),
+        fullName: joi.string().trim(),
+        phone: joi.string().trim(),
+        linkedinLink: joi.string().trim().uri(),
+        companyName: joi.string().trim(),
+        companyLogoId: joi.string().trim(),
+        companyWebsite: joi.string().trim().uri(),
+        employerPosition: joi.string().trim(),
+        cvId: joi.string().trim(),
+    })
+    .or('id', 'userId');
 
 export { userDetailsUpdate };
