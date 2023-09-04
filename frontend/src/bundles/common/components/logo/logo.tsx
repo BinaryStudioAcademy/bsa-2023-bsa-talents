a248473ac2711cccbe712e3ab379eb06f80aed57
vladjsp <vladjsp@gmail.com>
2023-09-04 13:46:01 +0300

    bt-222: + link to main page for logo

--- frontend/src/bundles/common/components/logo/logo.tsx
+++ frontend/src/bundles/common/components/logo/logo.tsx
@@ -2,7 +2,7 @@ import logoSvg from '~/assets/img/logo/logo.svg';
 import logoLabelSvg from '~/assets/img/logo/logo-label.svg';
 
 import { getValidClassNames } from '../../helpers/helpers.js';
-import { Grid } from '../components.js';
+import { Grid, Link } from '../components.js';
 import styles from './styles.module.scss';
 
 type Properties = {
@@ -15,27 +15,29 @@ const Logo = ({
     isCollapsed = false,
 }: Properties): JSX.Element => {
     return (
-        <Grid container className={getValidClassNames(styles.logo, className)}>
-            <Grid
-                item
-                alignItems="center"
-                className={getValidClassNames(
-                    styles.logoIcon,
-                    isCollapsed && styles.collapsed,
-                )}
-            >
-                <img src={logoSvg} alt="Logo-BSATalents" />
-            </Grid>
-
-            {!isCollapsed && (
+        <Grid container>
+            <Link to="/" className={getValidClassNames(styles.logo, className)}>
                 <Grid
                     item
                     alignItems="center"
-                    className={getValidClassNames(styles.logoLabel)}
+                    className={getValidClassNames(
+                        styles.logoIcon,
+                        isCollapsed && styles.collapsed,
+                    )}
                 >
-                    <img src={logoLabelSvg} alt="Label-BSATalents" />
+                    <img src={logoSvg} alt="Logo-BSATalents" />
                 </Grid>
-            )}
+
+                {!isCollapsed && (
+                    <Grid
+                        item
+                        alignItems="center"
+                        className={getValidClassNames(styles.logoLabel)}
+                    >
+                        <img src={logoLabelSvg} alt="Label-BSATalents" />
+                    </Grid>
+                )}
+            </Link>
         </Grid>
     );
 };
