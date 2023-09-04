1ac3e8ae400b96e94ad850f8d2c0513dec5945a8
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-09-04 14:33:45 +0300

    bt-116: * rename route talent path

--- frontend/src/bundles/common/enums/app-route.enum.ts
+++ frontend/src/bundles/common/enums/app-route.enum.ts
@@ -2,8 +2,8 @@ const AppRoute = {
     ROOT: '/',
     SIGN_IN: '/sign-in',
     SIGN_UP: '/sign-up',
-    SIGN_UP_TALENT: '/sign-up-talent',
-    SIGN_UP_TALENT_STEP: '/sign-up-talent/onboarding/step/:step',
+    TALENT: '/talent',
+    TALENT_STEP: '/talent/onboarding/step/:step',
     OTHER: '*',
 } as const;
 
