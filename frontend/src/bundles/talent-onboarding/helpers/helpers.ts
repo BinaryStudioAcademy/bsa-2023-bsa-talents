42f09d48fbfe048d4c9646c105b07c14d93dbaf3
savinaDaria <darianna697@gmail.com>
2023-09-04 14:58:56 +0300

    bt-132: * sort imports

--- frontend/src/bundles/talent-onboarding/helpers/helpers.ts
+++ frontend/src/bundles/talent-onboarding/helpers/helpers.ts
@@ -1,5 +1,5 @@
+export { getStepRoute } from './get-step-route/get-step-route.js';
 export {
     experienceYearsScaled,
     experienceYearsSliderMarks,
 } from './map-experience-years-to-marks.js';
-export { getStepRoute } from './get-step-route/get-step-route.js';
