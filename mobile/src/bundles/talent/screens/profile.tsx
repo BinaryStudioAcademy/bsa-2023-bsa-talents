dd6351f9954fd5cefb51d7a90b5265bce2132b6f
Tatiana Ubozhenko <tatiana.medvedeva.100@gmail.com>
2023-09-04 17:59:12 +0300

    bt-206: + handleProfileSubmit

--- mobile/src/bundles/talent/screens/profile.tsx
+++ mobile/src/bundles/talent/screens/profile.tsx
@@ -5,23 +5,34 @@ import {
     type TalentOnboardingScreenName,
     TalentOnboardingScreenNumber,
 } from '~/bundles/common/enums/enums';
-import { useAppRoute, useCallback } from '~/bundles/common/hooks/hooks';
+import {
+    useAppDispatch,
+    useAppRoute,
+    useCallback,
+} from '~/bundles/common/hooks/hooks';
 import { globalStyles } from '~/bundles/common/styles/styles';
 import { type ValueOf } from '~/bundles/common/types/types';
 import {
     NewAccountHeader,
     ProfileForm,
 } from '~/bundles/talent/components/components';
+//TODO store for talents
+import { actions as talentActions } from '~/bundles/talent/store';
+import { type TalentOnboardingProfileDto } from '~/bundles/talent/types/types';
 
 const Profile: React.FC = () => {
     const { name } = useAppRoute();
+    const dispatch = useAppDispatch();
+
     const stepTitle = name as ValueOf<typeof TalentOnboardingScreenName>;
     const stepNumber = TalentOnboardingScreenNumber[stepTitle];
 
-    const handleProfileSubmit = useCallback(() => {
-        // TODO: add submit logic
-        return null;
-    }, []);
+    const handleProfileSubmit = useCallback(
+        (payload: TalentOnboardingProfileDto): void => {
+            void dispatch(talentActions.loadProfile(payload));
+        },
+        [dispatch],
+    );
 
     return (
         <View style={globalStyles.flex1}>
