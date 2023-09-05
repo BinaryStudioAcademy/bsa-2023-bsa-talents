5d3fad4ec430db302cad9d72f61e0d85ae0366ec
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-09-06 00:45:52 +0300

    bt-215: * minor changes

--- frontend/src/bundles/talent-onboarding/pages/candidate-page/candidate-page.tsx
+++ frontend/src/bundles/talent-onboarding/pages/candidate-page/candidate-page.tsx
@@ -4,7 +4,7 @@ const CandidatePage: React.FC = () => {
     return (
         <>
             <BreadCrumbs />
-            <CandidateProfile isProfileOpen />
+            <CandidateProfile isProfileOpen={false} />
         </>
     );
 };
