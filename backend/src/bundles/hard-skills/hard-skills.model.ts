66a0a1efb1dbedafee2268e540a2c5b9568461bc
Matvii <42421305+Monyster@users.noreply.github.com>
2023-09-05 13:51:27 +0000

    bt-172: * fix

--- backend/src/bundles/hard-skills/hard-skills.model.ts
+++ backend/src/bundles/hard-skills/hard-skills.model.ts
@@ -1,12 +1,11 @@
-// import { Model } from 'objection';
+import { Model } from 'objection';
 
-// import { UserDetailsModel } from '~/bundles/user-details/user-details.js';
 import {
     AbstractModel,
     DatabaseTableName,
-    // HardSkillsTableColumn,
-    // TalentHardSkillsTableColumn,
-    // UserDetailsTableColumn,
+    HardSkillsTableColumn,
+    TalentHardSkillsTableColumn,
+    UserDetailsTableColumn,
 } from '~/common/packages/database/database.js';
 
 class HardSkillsModel extends AbstractModel {
@@ -17,18 +16,18 @@ class HardSkillsModel extends AbstractModel {
     }
 
     public static override relationMappings = {
-        // talents: {
-        //     relation: Model.ManyToManyRelation,
-        //     modelClass: UserDetailsModel,
-        //     join: {
-        //         from: `${DatabaseTableName.HARD_SKILLS}.${HardSkillsTableColumn.ID}`,
-        //         through: {
-        //             from: `${DatabaseTableName.TALENT_HARD_SKILLS}.${TalentHardSkillsTableColumn.HARD_SKILL_ID}`,
-        //             to: `${DatabaseTableName.TALENT_HARD_SKILLS}.${TalentHardSkillsTableColumn.USER_DETAILS_ID}`,
-        //         },
-        //         to: `${DatabaseTableName.USER_DETAILS}.${UserDetailsTableColumn.ID}`,
-        //     },
-        // },
+        talents: {
+            relation: Model.ManyToManyRelation,
+            modelClass: '',
+            join: {
+                from: `${DatabaseTableName.HARD_SKILLS}.${HardSkillsTableColumn.ID}`,
+                through: {
+                    from: `${DatabaseTableName.TALENT_HARD_SKILLS}.${TalentHardSkillsTableColumn.HARD_SKILL_ID}`,
+                    to: `${DatabaseTableName.TALENT_HARD_SKILLS}.${TalentHardSkillsTableColumn.USER_DETAILS_ID}`,
+                },
+                to: `${DatabaseTableName.USER_DETAILS}.${UserDetailsTableColumn.ID}`,
+            },
+        },
     };
 }
 
