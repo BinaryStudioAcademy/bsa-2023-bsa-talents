073ce9dbe9d1818e610839b3908d3a770320efdb
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-09-05 15:18:40 +0300

    bt-231: + animation to slider

--- mobile/src/navigations/onboarding-navigator/components/steps/steps.tsx
+++ mobile/src/navigations/onboarding-navigator/components/steps/steps.tsx
@@ -1,8 +1,13 @@
 import {
     type DrawerContentComponentProps,
     DrawerContentScrollView,
+    useDrawerProgress,
 } from '@react-navigation/drawer';
 import React from 'react';
+import Animated, {
+    interpolate,
+    useAnimatedStyle,
+} from 'react-native-reanimated';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { Pressable, Text, View } from '~/bundles/common/components/components';
@@ -20,10 +25,43 @@ import {
 } from '~/bundles/common/types/types';
 
 import { Step } from '../components';
+import { ANIMATION_VALUES } from './constants/constants';
 import { styles } from './styles';
+
+const {
+    ICON_FINAL_X_COORDINATES,
+    TEXT_FINAL_Y_COORDINATES,
+    ICON_INITIAL_X_COORDINATES,
+    TEXT_INITIAL_Y_COORDINATES,
+    INITIAL_PROGRESS_VALUE,
+    FINAL_PROGRESS_VALUE,
+} = ANIMATION_VALUES;
 
 const Steps: React.FC<DrawerContentComponentProps> = (props) => {
     const { navigation, state } = props;
+    const progress = useDrawerProgress();
+
+    const textAnimatedStyle = useAnimatedStyle(() => {
+        const translateY = interpolate(
+            progress.value,
+            [INITIAL_PROGRESS_VALUE, FINAL_PROGRESS_VALUE],
+            [TEXT_INITIAL_Y_COORDINATES, TEXT_FINAL_Y_COORDINATES],
+        );
+        return {
+            transform: [{ translateY }],
+        };
+    });
+
+    const iconAnimatedStyle = useAnimatedStyle(() => {
+        const translateX = interpolate(
+            progress.value,
+            [INITIAL_PROGRESS_VALUE, FINAL_PROGRESS_VALUE],
+            [ICON_INITIAL_X_COORDINATES, ICON_FINAL_X_COORDINATES],
+        );
+        return {
+            transform: [{ translateX }],
+        };
+    });
 
     return (
         <DrawerContentScrollView
@@ -36,44 +74,48 @@ const Steps: React.FC<DrawerContentComponentProps> = (props) => {
                 }}
                 style={styles.button}
             >
-                <Icon name={IconName.CLOSE} size={40} color={Color.INPUT} />
+                <Animated.View style={iconAnimatedStyle}>
+                    <Icon name={IconName.CLOSE} size={40} color={Color.INPUT} />
+                </Animated.View>
             </Pressable>
-            <Text category={TextCategory.H2} style={styles.title}>
-                Steps
-            </Text>
-            <View style={styles.verticalLine} />
+            <Animated.View style={textAnimatedStyle}>
+                <Text category={TextCategory.H2} style={styles.title}>
+                    Steps
+                </Text>
+                <View style={styles.verticalLine} />
 
-            {state.routes.map((route, index) => {
-                // TODO: add logic to step completion
-                const isFocused = state.index === index;
-                const routeName = route.name as ValueOf<
-                    typeof TalentOnboardingScreenName
-                >;
-                const stepNumber = TalentOnboardingScreenNumber[routeName];
-                const { stepState } =
-                    route.params as TalentOnboardingRouteProperties;
+                {state.routes.map((route, index) => {
+                    // TODO: add logic to step completion
+                    const isFocused = state.index === index;
+                    const routeName = route.name as ValueOf<
+                        typeof TalentOnboardingScreenName
+                    >;
+                    const stepNumber = TalentOnboardingScreenNumber[routeName];
+                    const { stepState } =
+                        route.params as TalentOnboardingRouteProperties;
 
-                const onPress = (): void => {
-                    const event = navigation.emit({
-                        type: 'drawerItemPress',
-                        target: route.key,
-                        canPreventDefault: true,
-                    });
-                    if (!isFocused && !event.defaultPrevented) {
-                        navigation.navigate(routeName);
-                    }
-                };
+                    const onPress = (): void => {
+                        const event = navigation.emit({
+                            type: 'drawerItemPress',
+                            target: route.key,
+                            canPreventDefault: true,
+                        });
+                        if (!isFocused && !event.defaultPrevented) {
+                            navigation.navigate(routeName);
+                        }
+                    };
 
-                return (
-                    <Step
-                        key={route.key}
-                        routeName={routeName}
-                        onPress={onPress}
-                        stepNumber={stepNumber}
-                        stepState={stepState}
-                    />
-                );
-            })}
+                    return (
+                        <Step
+                            key={route.key}
+                            routeName={routeName}
+                            onPress={onPress}
+                            stepNumber={stepNumber}
+                            stepState={stepState}
+                        />
+                    );
+                })}
+            </Animated.View>
         </DrawerContentScrollView>
     );
 };
