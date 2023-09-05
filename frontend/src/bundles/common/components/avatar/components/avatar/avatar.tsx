9dce95cfb32332ba59d8c1327c0383855682c985
JemiJe <jeremiah7480@gmail.com>
2023-09-05 22:08:11 +0300

    bt-252: + Avatar, HeaderAvatar

---
+++ frontend/src/bundles/common/components/avatar/components/avatar/avatar.tsx
@@ -0,0 +1,31 @@
+import { Avatar as MUIAvatar, type AvatarProps } from '@mui/material';
+
+import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
+
+import styles from './styles.module.scss';
+
+type Properties = AvatarProps & {
+    small?: boolean;
+};
+
+const Avatar: React.FC<Properties> = ({
+    small = false,
+    className,
+    ...props
+}) => {
+    return (
+        <MUIAvatar
+            className={getValidClassNames(
+                className,
+                styles.avatar,
+                small ? styles.small : '',
+            )}
+            {...props}
+        >
+            {/* default icon/text */}
+        </MUIAvatar>
+    );
+};
+
+export { type Properties as AvatarProperties };
+export { Avatar };
