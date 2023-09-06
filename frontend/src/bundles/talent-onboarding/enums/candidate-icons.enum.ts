4aa2210d85e7a75b26ed54d40ed420ded6e09bf1
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-09-07 00:48:55 +0300

    bt-215: * resolve new conflict

--- frontend/src/bundles/talent-onboarding/enums/candidate-icons.enum.ts
+++ frontend/src/bundles/talent-onboarding/enums/candidate-icons.enum.ts
@@ -1,12 +1,14 @@
-import { Language } from '@mui/icons-material';
-import { Paid } from '@mui/icons-material';
-import { SwitchCamera } from '@mui/icons-material';
-import { Email } from '@mui/icons-material';
-import { QuestionAnswer } from '@mui/icons-material';
-import { PhoneAndroid } from '@mui/icons-material';
-import { CheckCircle } from '@mui/icons-material';
-import { DoNotDisturbOn } from '@mui/icons-material';
-import { Telegram } from '@mui/icons-material';
+import {
+    CheckCircle,
+    DoNotDisturbOn,
+    Email,
+    Language,
+    Paid,
+    PhoneAndroid,
+    QuestionAnswer,
+    SwitchCamera,
+    Telegram,
+} from '@mui/icons-material';
 
 const CandidateIcons = {
     SALARY: Paid,
