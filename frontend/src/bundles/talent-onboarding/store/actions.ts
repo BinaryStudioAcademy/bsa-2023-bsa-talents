9115510c7a941b162277fd18b28608d26f6903d2
savinaDaria <darianna697@gmail.com>
2023-09-07 02:25:21 +0300

    bt-224: * change store

--- frontend/src/bundles/talent-onboarding/store/actions.ts
+++ frontend/src/bundles/talent-onboarding/store/actions.ts
@@ -1,36 +1,16 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import {
-    type ContactsCVStepDto,
-    type ProfileStepDto,
-    type SkillsStepDto,
-} from 'shared/build/index.js';
+import { type UserDetailsUpdateRequestDto } from 'shared/build/index.js';
 
 import { type AsyncThunkConfig } from '~/bundles/common/types/types.js';
 
 import { name as sliceName } from './slice.js';
 
-const profileStep = createAsyncThunk<
-    ProfileStepDto,
-    ProfileStepDto,
-    AsyncThunkConfig
->(`${sliceName}/profile-step`, (registerPayload) => {
-    return registerPayload;
-});
-
-const skillsStep = createAsyncThunk<
-    SkillsStepDto,
-    SkillsStepDto,
-    AsyncThunkConfig
->(`${sliceName}/skills-step`, (registerPayload) => {
-    return registerPayload;
-});
-
-const contactsCVStep = createAsyncThunk<
-    ContactsCVStepDto,
-    ContactsCVStepDto,
+const updateTalentDetails = createAsyncThunk<
+    UserDetailsUpdateRequestDto,
+    UserDetailsUpdateRequestDto,
     AsyncThunkConfig
 >(`${sliceName}/contacts-cv-step`, (registerPayload) => {
     return registerPayload;
 });
 
-export { contactsCVStep, profileStep, skillsStep };
+export { updateTalentDetails };
