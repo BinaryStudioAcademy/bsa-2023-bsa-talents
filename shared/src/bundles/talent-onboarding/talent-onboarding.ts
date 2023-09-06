a0dc86030c5ce74b20e50a827efb2eae9d9eb991
Chichik Anastasiia <chichiknastya@gmail.com>
2023-09-06 11:46:24 +0300

    bt-169: * solve bugs

--- shared/src/bundles/talent-onboarding/talent-onboarding.ts
+++ shared/src/bundles/talent-onboarding/talent-onboarding.ts
@@ -4,8 +4,5 @@ export {
     SkillsStepValidationMessage,
     SkillsStepValidationRule,
 } from './enums/enums.js';
-export { type ProfileStepDto, type SkillsStepDto } from './types/types.js';
-export {
-    ProfileStepValidationSchema,
-    SkillsStepValidationSchema,
-} from './validation-schemas/validation-schemas.js';
+export { type ProfileStepDto } from './types/types.js';
+export { ProfileStepValidationSchema } from './validation-schemas/validation-schemas.js';
