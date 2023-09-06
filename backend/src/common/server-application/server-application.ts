1970def2a92e2b373a7f886bc81e21a27b1fca11
JoshDagat <josuer.online.work@gmail.com>
2023-09-06 18:55:00 +0800

    bt-250: + Registered file controller on application server

--- backend/src/common/server-application/server-application.ts
+++ backend/src/common/server-application/server-application.ts
@@ -1,4 +1,5 @@
 import { authController } from '~/bundles/auth/auth.js';
+import { fileController } from '~/bundles/files/files.js';
 import { userController } from '~/bundles/users/users.js';
 import { config, database, logger } from '~/common/packages/packages.js';
 
@@ -10,6 +11,7 @@ const apiV1 = new ServerAppApiBase(
     config,
     ...authController.routes,
     ...userController.routes,
+    ...fileController.routes,
 );
 const serverApp = new ServerAppBase({
     config,
