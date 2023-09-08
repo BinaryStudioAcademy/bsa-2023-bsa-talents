891be3dde6f8d83ef4a764dc77ff02083910ea8c
mchornomaz <chornomaz.maksym@gmail.com>
2023-09-08 23:32:35 +0300

    bt-310: * fixed password validation messages in SignIn and SignUp forms

--- shared/src/bundles/users/enums/user-validation-message.enum.ts
+++ shared/src/bundles/users/enums/user-validation-message.enum.ts
@@ -3,6 +3,8 @@ const UserValidationMessage = {
     EMAIL_WRONG: 'Email is wrong',
     PASSWORD_WRONG:
         'Password should contain at least one uppercase letter (A-Z), one lowercase letter (a-z), one digit and one special character ',
+    PASSWORD_REQUIRE: 'Password is not allowed to be empty',
+    PASSWORD_SHORT: 'Password length must be at least 8 characters long',
 } as const;
 
 export { UserValidationMessage };
