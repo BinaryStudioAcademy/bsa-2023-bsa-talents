fcdc6dc04c0146603157eb22abd998d593214338
savinaDaria <darianna697@gmail.com>
2023-09-11 11:35:03 +0300

    bt-329: * change to undefined

--- shared/src/bundles/talent-onboarding/types/profile-step/profile-step-dto.ts
+++ shared/src/bundles/talent-onboarding/types/profile-step/profile-step-dto.ts
@@ -1,6 +1,6 @@
 type ProfileStepDto = {
     profileName: string;
-    salaryExpectation: number;
+    salaryExpectation: number | undefined;
     jobTitle: string;
     location: string;
     experienceYears: number;
