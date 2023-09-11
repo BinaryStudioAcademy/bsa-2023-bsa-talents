4e8e2e209113203b50b8bfbba65abeca1361a45e
Pavlo Tarhonskyi <ptarhonskyi@gmail.com>
2023-09-11 17:31:24 +0300

    bt-349: * Fix typos

--- frontend/src/bundles/talent-onboarding/components/profile-step/profile-step.tsx
+++ frontend/src/bundles/talent-onboarding/components/profile-step/profile-step.tsx
@@ -249,7 +249,7 @@ const ProfileStep: React.FC = () => {
             </FormControl>
             <FormControl className={styles.formControl}>
                 <FormLabel className={styles.formLabel} required>
-                    <Typography variant={'label'}>Current location</Typography>
+                    <Typography variant={'label'}>Current Location</Typography>
                 </FormLabel>
                 <Select
                     control={control}
