bf9bb1abba506743575e3f02f94e182cccf3c83a
savinaDaria <darianna697@gmail.com>
2023-09-11 19:23:45 +0300

    bt-220: * change path name

--- frontend/src/bundles/talent-onboarding/enums/steps.enum.ts
+++ frontend/src/bundles/talent-onboarding/enums/steps.enum.ts
@@ -10,7 +10,7 @@ const StepsRoute = {
     STEP_01: 'profile',
     STEP_02: 'bsa-badges',
     STEP_03: 'skills-and-projects',
-    STEP_04: 'cv-and-ontacts',
+    STEP_04: 'cv-and-contacts',
     STEP_05: 'preview',
 } as const;
 
