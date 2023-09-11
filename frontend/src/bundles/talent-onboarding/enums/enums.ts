ce7f9e67a7526da74a86a54c2695ddd5bdbbcac8
JoshDagat <josuer.online.work@gmail.com>
2023-09-11 23:08:20 +0800

    bt-358: * Changes to contacts-cv-step enums

--- frontend/src/bundles/talent-onboarding/enums/enums.ts
+++ frontend/src/bundles/talent-onboarding/enums/enums.ts
@@ -1,5 +1,5 @@
-export { ContactsCVStepValidationMessage } from './contacts-cv-step/contacts-cv-step.validation-message.js';
-export { ContactsCVStepValidationRule } from './contacts-cv-step/contacts-cv-step.validation-rule.js';
+export { ContactsCVStepValidationMessages } from './contacts-cv-step/contacts-cv-step.validation-messages.enum.js';
+export { ContactsCVStepValidationRules } from './contacts-cv-step/contacts-cv-step.validation-rules.enum.js';
 export { Steps } from './steps.enum.js';
 export {
     CountryList,
