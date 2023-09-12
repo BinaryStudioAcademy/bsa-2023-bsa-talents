ccee99244402cb373fc5583335e1a46f7780d5c4
JemiJe <jeremiah7480@gmail.com>
2023-09-13 00:09:03 +0300

    bt-160: + responsive chatlist part one

--- frontend/src/bundles/chat/pages/chats/chats-page.tsx
+++ frontend/src/bundles/chat/pages/chats/chats-page.tsx
@@ -1,3 +1,7 @@
+import { Close, PersonSearch } from '@mui/icons-material';
+import { IconButton, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
 import {
     ChatHeader,
     ChatList,
@@ -5,6 +9,12 @@ import {
     MessageList,
 } from '~/bundles/chat/components/components.js';
 import { Grid, Typography } from '~/bundles/common/components/components.js';
+import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
+import {
+    useCallback,
+    useEffect,
+    useState,
+} from '~/bundles/common/hooks/hooks.js';
 
 // import { useCallback } from '~/bundles/common/hooks/hooks.js';
 import { items, messages } from '../../mock-data/mock-data.js';
@@ -14,18 +24,61 @@ const ChatsPage: React.FC = () => {
     // const sendMessage = useCallback((message: string) => {
     // }, []);
 
+    const theme = useTheme();
+    const isScreenMedium = useMediaQuery(theme.breakpoints.down('md'));
+
+    const [isOpenChatList, setIsOpenChatList] = useState(false);
+
+    const handleOpenChatListButton = useCallback(() => {
+        setIsOpenChatList(!isOpenChatList);
+    }, [isOpenChatList]);
+
+    useEffect(() => {
+        setIsOpenChatList(false);
+    }, [isScreenMedium]);
+
     return (
         <Grid container direction="column">
             <Typography variant="h4" className={styles.header}>
                 Chats
             </Typography>
-            <Grid container wrap="nowrap" className={styles.chatWrapper}>
-                <Grid className={styles.chatList}>
-                    <ChatList chatItems={items} />
-                </Grid>
-                <Grid container flexGrow={1} direction="column">
+            <Grid
+                container
+                wrap="nowrap"
+                className={getValidClassNames(
+                    styles.chatWrapper,
+                    isOpenChatList && styles.chatWrapperOnChatListOpened,
+                )}
+            >
+                {(!isScreenMedium || isOpenChatList) && (
+                    <Grid
+                        className={getValidClassNames(
+                            styles.chatList,
+                            isOpenChatList && styles.chatListOpened,
+                        )}
+                    >
+                        <ChatList chatItems={items} />
+                    </Grid>
+                )}
+                <Grid
+                    container
+                    flexGrow={1}
+                    direction="column"
+                    className={styles.chatWindow}
+                >
+                    {isScreenMedium && (
+                        <div className={styles.openChatListButtonWrapper}>
+                            <IconButton onClick={handleOpenChatListButton}>
+                                {isOpenChatList ? (
+                                    <Close fontSize="large" />
+                                ) : (
+                                    <PersonSearch fontSize="large" />
+                                )}
+                            </IconButton>
+                        </div>
+                    )}
                     <ChatHeader
-                        title="hello"
+                        title={'' + isScreenMedium}
                         isOnline
                         className={styles.chatHeader}
                     />
