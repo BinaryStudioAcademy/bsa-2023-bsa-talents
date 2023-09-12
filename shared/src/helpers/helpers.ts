0eb19145d01233c5aa443a9b9a32c6756607e358
Paul <rayver2012@gmail.com>
2023-09-12 14:16:09 +0300

    bt-216: * Fix folder name

--- shared/src/helpers/helpers.ts
+++ shared/src/helpers/helpers.ts
@@ -1,4 +1,4 @@
 export { configureString } from './configure-string/configure-string.helper.js';
-export { getNestedProperty } from './getNestedProperty/get-nested-property.js';
+export { getNestedProperty } from './get-nested-property/get-nested-property.js';
 export { createNumberRangeArray } from './talent-onboarding/create-number-range.js';
 export { getAvatarInitials } from './user/get-avatar-initials.js';
