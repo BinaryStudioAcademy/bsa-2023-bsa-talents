1c0a05bcaa9666999f4b832f8bb1ace4ac481b8e
nika-sirak <7nicka@gmail.com>
2023-09-12 19:04:29 +0100

    bt-356: - remove step labels enum and add fn to parse label for layout

--- frontend/src/bundles/talent-onboarding/enums/enums.ts
+++ frontend/src/bundles/talent-onboarding/enums/enums.ts
@@ -1,6 +1,5 @@
 export { ContactsCVStepValidationMessage } from './contacts-cv-step/contacts-cv-step.validation-message.js';
 export { ContactsCVStepValidationRule } from './contacts-cv-step/contacts-cv-step.validation-rule.js';
-export { StepLabels } from './steps.enum.js';
 export {
     CountryList,
     EmploymentType,
--- frontend/src/bundles/talent-onboarding/enums/steps.enum.ts
+++
@@ -1,9 +0,0 @@
-const StepLabels = {
-    STEP_01: 'Profile',
-    STEP_02: 'BSA badges',
-    STEP_03: 'Skills and projects',
-    STEP_04: 'CV and contacts',
-    STEP_05: 'Preview',
-} as const;
-
-export { StepLabels };
