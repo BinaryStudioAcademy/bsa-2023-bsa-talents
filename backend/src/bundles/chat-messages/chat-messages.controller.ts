85edf10eb6b2d461c1560e3b1e4fac9af7f79340
Matvey V <main.monyster@gmail.com>
2023-09-13 20:34:11 +0300

    bt-151: + swagger docs

--- backend/src/bundles/chat-messages/chat-messages.controller.ts
+++ backend/src/bundles/chat-messages/chat-messages.controller.ts
@@ -21,12 +21,31 @@ import { ChatMessagesCreateValidationSchema } from './validation-schemas/validat
  *        scheme: bearer
  *        bearerFormat: JWT
  *    schemas:
- *      UserDetails:
+ *      ChatMessage:
  *        type: object
  *        properties:
  *          id:
- *            format: uuid #Example: '550e8400-e29b-41d4-a716-446655440000'
+ *            format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
  *            type: string
+ *            description: The unique identifier for the chat message.
+ *          senderId:
+ *            format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+ *            type: string
+ *            description: The ID of the message sender.
+ *          receiverId:
+ *            format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+ *            type: string
+ *            description: The ID of the message receiver.
+ *          chatId:
+ *            format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+ *            type: string
+ *            description: The ID of the chat to which the message belongs.
+ *          message:
+ *            type: string
+ *            description: The content of the chat message.
+ *          isRead:
+ *            type: boolean
+ *            description: Indicates whether the message has been read (true) or not (false).
  */
 class ChatMessagesController extends ControllerBase {
     private chatMessagesService: ChatMessagesService;
@@ -46,6 +65,17 @@ class ChatMessagesController extends ControllerBase {
         });
 
         this.addRoute({
+            path: ChatMessagesApiPath.$CHAT_ID,
+            method: 'GET',
+            handler: (options) =>
+                this.getAllByChatId(
+                    options as ApiHandlerOptions<{
+                        params: { chatId: string };
+                    }>,
+                ),
+        });
+
+        this.addRoute({
             path: ChatMessagesApiPath.ROOT,
             method: 'POST',
             validation: {
@@ -55,17 +85,6 @@ class ChatMessagesController extends ControllerBase {
                 this.create(
                     options as ApiHandlerOptions<{
                         body: ChatMessagesCreateRequestDto;
-                    }>,
-                ),
-        });
-
-        this.addRoute({
-            path: ChatMessagesApiPath.$CHAT_ID,
-            method: 'GET',
-            handler: (options) =>
-                this.getAllByChatId(
-                    options as ApiHandlerOptions<{
-                        params: { chatId: string };
                     }>,
                 ),
         });
@@ -82,6 +101,25 @@ class ChatMessagesController extends ControllerBase {
         });
     }
 
+    /**
+     * @swagger
+     * /chat-messages/:
+     *   get:
+     *     tags:
+     *       - "Chat Messages"
+     *     description: Retrieves all chat messages
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: Successful operation
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 $ref: '#/components/schemas/ChatMessage'
+     */
     private async getAll(): Promise<ApiHandlerResponse> {
         return {
             status: HttpCode.OK,
@@ -89,17 +127,33 @@ class ChatMessagesController extends ControllerBase {
         };
     }
 
-    private async create(
-        options: ApiHandlerOptions<{
-            body: ChatMessagesCreateRequestDto;
-        }>,
-    ): Promise<ApiHandlerResponse> {
-        return {
-            status: HttpCode.OK,
-            payload: await this.chatMessagesService.create(options.body),
-        };
-    }
-
+    /**
+     * @swagger
+     * /chat-messages/{chatId}:
+     *   get:
+     *     tags:
+     *       - "Chat Messages"
+     *     description: Retrieves all chat messages for a specific chat by chatId
+     *     security:
+     *       - bearerAuth: []
+     *     parameters:
+     *       - name: chatId
+     *         in: path
+     *         description: The ID of the chat to retrieve messages for.
+     *         required: true
+     *         schema:
+     *           format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+     *           type: string
+     *     responses:
+     *       200:
+     *         description: Successful retrieval of chat messages
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 $ref: '#/components/schemas/ChatMessage'
+     */
     private async getAllByChatId(
         options: ApiHandlerOptions<{
             params: { chatId: string };
@@ -113,6 +167,87 @@ class ChatMessagesController extends ControllerBase {
         };
     }
 
+    /**
+     * @swagger
+     * /chat-messages/:
+     *   post:
+     *     tags:
+     *       - "Chat Messages"
+     *     description: Creates a new chat message
+     *     security:
+     *       - bearerAuth: []
+     *     requestBody:
+     *       description: Chat message creation object
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/ChatMessagesCreateRequestDto'
+     *     responses:
+     *       200:
+     *         description: Successful creation of a chat message
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/ChatMessage'
+     * components:
+     *   schemas:
+     *     ChatMessagesCreateRequestDto:
+     *       type: object
+     *       properties:
+     *         senderId:
+     *           format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+     *           type: string
+     *           description: The ID of the sender.
+     *         receiverId:
+     *           format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+     *           type: string
+     *           description: The ID of the receiver.
+     *         chatId:
+     *           format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+     *           type: string
+     *           description: The ID of the chat.
+     *         message:
+     *           type: string
+     *           description: The message content.
+     *
+     */
+    private async create(
+        options: ApiHandlerOptions<{
+            body: ChatMessagesCreateRequestDto;
+        }>,
+    ): Promise<ApiHandlerResponse> {
+        return {
+            status: HttpCode.OK,
+            payload: await this.chatMessagesService.create(options.body),
+        };
+    }
+
+    /**
+     * @swagger
+     * /chat-messages/{messageId}:
+     *   patch:
+     *     tags:
+     *       - "Chat Messages"
+     *     description: Marks a chat message as read by messageId
+     *     security:
+     *       - bearerAuth: []
+     *     parameters:
+     *       - name: messageId
+     *         in: path
+     *         description: The ID of the chat message to mark as read.
+     *         required: true
+     *         schema:
+     *           format: uuid # Example: '550e8400-e29b-41d4-a716-446655440000'
+     *           type: string
+     *     responses:
+     *       200:
+     *         description: Successful marking of a chat message as read
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/ChatMessage'
+     */
     private async read(
         options: ApiHandlerOptions<{
             params: { messageId: string };
