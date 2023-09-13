d0bf0e3baa0dc3df194fb72273a9badf92dd2854
vladjsp <vladjsp@gmail.com>
2023-09-13 20:54:52 +0300

    bt-258: * update years of experience select values

--- frontend/src/bundles/employers/enums/enums.ts
+++ frontend/src/bundles/employers/enums/enums.ts
@@ -1,11 +1,11 @@
 export { BsaBadges } from './bsa-badges.enum.js';
 export { BsaCharacteristics } from './bsa-characteristics.enum.js';
 export { BsaProject } from './bsa-project.enum.js';
+export { ExperienceYears } from './experience-years.enum.js';
 export { HardSkills } from './hard-skills.enum.js';
 export {
     CountryList,
     EmploymentType,
     EnglishLevel,
-    ExperienceYears,
     JobTitle,
 } from '~/bundles/talent-onboarding/enums/enums.js';
