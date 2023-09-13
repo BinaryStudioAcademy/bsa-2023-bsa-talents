86678a6aabc68a7da3180f1729ede8b8ac300b29
dobroillya <dobroillya@gmail.com>
2023-09-13 20:12:00 +0300

    bt-330: * return to 65 width

--- mobile/src/navigations/onboarding-navigator/components/header/styles.ts
+++ mobile/src/navigations/onboarding-navigator/components/header/styles.ts
@@ -16,8 +16,8 @@ const styles = StyleSheet.create({
         color: '#fff',
     },
     steps: {
-        width: 68,
-        height: 68,
+        width: 65,
+        height: 65,
         borderRadius: 100,
         backgroundColor: '#d6dce8',
         position: 'absolute',
