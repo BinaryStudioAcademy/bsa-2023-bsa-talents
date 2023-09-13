bd0f87f415e911af24d2819a2d23327ce9497af2
Matvey V <main.monyster@gmail.com>
2023-09-13 20:43:57 +0300

    bt-151: * uuid in validation

--- shared/src/bundles/chat-messages/validation-schemas/chat-messages-create.validation-schema.ts
+++ shared/src/bundles/chat-messages/validation-schemas/chat-messages-create.validation-schema.ts
@@ -3,9 +3,9 @@ import joi from 'joi';
 import { type ChatMessagesCreateRequestDto } from '../chat-messages.js';
 
 const chatMessagesCreate = joi.object<ChatMessagesCreateRequestDto, true>({
-    senderId: joi.string().trim().required(),
-    receiverId: joi.string().trim().required(),
-    chatId: joi.string().trim().required(),
+    senderId: joi.string().trim().uuid().required(),
+    receiverId: joi.string().trim().uuid().required(),
+    chatId: joi.string().trim().uuid().required(),
     message: joi.string().trim().required(),
 });
 
