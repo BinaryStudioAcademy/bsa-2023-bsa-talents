de18b83a30dccda8b545d37861a254fbb7b9294f
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-09-13 17:47:58 +0300

    bt-215: - global styles

--- frontend/src/bundles/talent-onboarding/components/bread-crumbs/bread-crumbs.tsx
+++ frontend/src/bundles/talent-onboarding/components/bread-crumbs/bread-crumbs.tsx
@@ -1,19 +1,23 @@
+import { useCallback } from 'react';
+
 import RoundedArrow from '~/assets/img/rounded-arrow.svg';
 import { Link } from '~/bundles/common/components/components.js';
 import { AppRoute } from '~/bundles/common/enums/app-route.enum.js';
-import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
 
 import styles from './styles.module.scss';
 
 const BreadCrumbs: React.FC = () => {
+    const linkClass = useCallback(
+        ({ isActive }: { isActive: boolean }) =>
+            isActive ? `${styles.active}` : `${styles.inactive}`,
+        [],
+    );
+
     return (
         <nav className={styles.breadCrumbs}>
             <ul className={styles.breadCrumbsList}>
-                <li>
-                    <Link
-                        to={AppRoute.CANDIDATES}
-                        className={styles.breadCrumbLink}
-                    >
+                <li className={styles.breadCrumbsListItem}>
+                    <Link to={AppRoute.CANDIDATES} className={linkClass}>
                         Candidates
                     </Link>
                 </li>
@@ -24,13 +28,7 @@ const BreadCrumbs: React.FC = () => {
                 />
                 <li>
                     {/*TODO: change link 'to' and link text */}
-                    <Link
-                        to={AppRoute.CANDIDATE}
-                        className={getValidClassNames(
-                            styles.breadCrumbLink,
-                            styles.breadCrumbLinkActive,
-                        )}
-                    >
+                    <Link to={AppRoute.CANDIDATE} className={linkClass}>
                         Middle Python Developer
                     </Link>
                 </li>
