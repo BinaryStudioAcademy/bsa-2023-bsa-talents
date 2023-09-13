98c7b4365752b5dbf86671500d0b5eb031f0392d
savinaDaria <darianna697@gmail.com>
2023-09-13 14:14:34 +0300

    bt-258: * debounce work in any input change

--- frontend/src/bundles/employers/store/actions.ts
+++ frontend/src/bundles/employers/store/actions.ts
@@ -2,25 +2,17 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { type AsyncThunkConfig } from '~/bundles/common/types/types.js';
 
-import { debounce } from '../helpers/helpers.js';
 import { type EmployeesFiltersDto } from '../types/employees-filters-dto.js';
 import { name as sliceName } from './slice.js';
-
-const SEND_DELAY = 2000;
-const fetchCandidates = (filters: EmployeesFiltersDto): EmployeesFiltersDto => {
-    // TODO: add API call logic here...
-    // console.log('api call', filters);
-    return filters;
-};
-
-const debouncedFetch = debounce(fetchCandidates, SEND_DELAY);
 
 const searchCandidates = createAsyncThunk<
     EmployeesFiltersDto,
     EmployeesFiltersDto,
     AsyncThunkConfig
 >(`${sliceName}/search-candidates`, (filters) => {
-    debouncedFetch(filters);
+    // Call your async backend method here if needed
+    // For now, we're just returning the filters
+    //console.log('api call', filters);
     return filters;
 });
 
--- frontend/src/bundles/employers/store/slice.ts
+++ frontend/src/bundles/employers/store/slice.ts
@@ -3,14 +3,18 @@ import { createSlice } from '@reduxjs/toolkit';
 import { DataStatus } from '~/bundles/common/enums/enums.js';
 import { type ValueOf } from '~/bundles/common/types/types.js';
 
+import { DEFAULT_EMPLOYEES_FILTERS_PAYLOAD } from '../constants/constants.js';
+import { type EmployeesFiltersDto } from '../types/employees-filters-dto.js';
 import { searchCandidates } from './actions.js';
 
 type State = {
     dataStatus: ValueOf<typeof DataStatus>;
+    filters: EmployeesFiltersDto;
 };
 
 const initialState: State = {
     dataStatus: DataStatus.IDLE,
+    filters: DEFAULT_EMPLOYEES_FILTERS_PAYLOAD,
 };
 
 const { reducer, actions, name } = createSlice({
@@ -18,8 +22,9 @@ const { reducer, actions, name } = createSlice({
     name: 'employers',
     reducers: {},
     extraReducers(builder) {
-        builder.addCase(searchCandidates.fulfilled, (state) => {
+        builder.addCase(searchCandidates.fulfilled, (state, action) => {
             state.dataStatus = DataStatus.FULFILLED;
+            state.filters = action.payload;
         });
         builder.addCase(searchCandidates.pending, (state) => {
             state.dataStatus = DataStatus.PENDING;
