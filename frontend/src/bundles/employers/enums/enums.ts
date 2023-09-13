a0dfd40970ac7c40da3cf8fd4aa274261e6b9d33
savinaDaria <darianna697@gmail.com>
2023-09-14 00:32:03 +0300

    bt-258: - remove useless enum

--- frontend/src/bundles/employers/enums/enums.ts
+++ frontend/src/bundles/employers/enums/enums.ts
@@ -2,10 +2,9 @@ export { BsaBadges } from './bsa-badges.enum.js';
 export { BsaCharacteristics } from './bsa-characteristics.enum.js';
 export { BsaProject } from './bsa-project.enum.js';
 export { ExperienceYears } from './experience-years.enum.js';
-export { HardSkills } from './hard-skills.enum.js';
 export {
     CountryList,
     EmploymentType,
     EnglishLevel,
     JobTitle,
-} from '~/bundles/talent-onboarding/enums/enums.js';
+} from 'shared/build/index.js';
