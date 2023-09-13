c7a8018a6c7ce3132cf2523772aa72978cea15f6
Chichik Anastasiia <chichiknastya@gmail.com>
2023-09-13 12:06:24 +0300

    bt-295: * change import

--- frontend/src/bundles/employer/enums/enums.ts
+++ frontend/src/bundles/employer/enums/enums.ts
@@ -1,3 +1,3 @@
 export { EmployerRegistrationValidationRule } from './employer-registration-form/employer-registartion-form.validation-rule.js';
 export { EmployerRegistrationValidationMessage } from './employer-registration-form/employer-registration-form.validation-message.js';
-export { CountryList } from 'shared/build/index.js';
+export { CountryList } from 'shared/build/shared/src/index.js';
