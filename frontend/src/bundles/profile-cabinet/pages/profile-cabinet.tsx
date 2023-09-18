fcec7d47a364667b8a46672956f54bd5e773484c
savinaDaria <darianna697@gmail.com>
2023-09-18 03:25:20 +0300

    bt-468: + add dispatch details on reload

--- frontend/src/bundles/profile-cabinet/pages/profile-cabinet.tsx
+++ frontend/src/bundles/profile-cabinet/pages/profile-cabinet.tsx
@@ -1,3 +1,4 @@
+import { actions as storeActions } from '~/app/store/app.js';
 import {
     Button,
     FormControl,
@@ -8,51 +9,84 @@ import {
     Tabs,
     Typography,
 } from '~/bundles/common/components/components.js';
-import { FormSubmitProvider } from '~/bundles/common/context/context.js';
+import { useFormSubmit } from '~/bundles/common/context/context.js';
+import {
+    useAppDispatch,
+    useAppSelector,
+    useCallback,
+    useEffect,
+} from '~/bundles/common/hooks/hooks.js';
+import { actions } from '~/bundles/talent-onboarding/store/talent-onboarding.js';
+import { type RootReducer } from '~/framework/store/store.js';
+import { NotificationType } from '~/services/notification/enums/notification-types.enum.js';
 
 import styles from './styles.module.scss';
 
 const ProfileCabinet: React.FC = () => {
+    const { submitForm } = useFormSubmit();
+    const dispatch = useAppDispatch();
+    const { currentUser } = useAppSelector((state: RootReducer) => state.auth);
+    useEffect(() => {
+        void dispatch(
+            actions.getTalentDetails({
+                userId: currentUser?.id,
+            }),
+        );
+    }, [currentUser?.id, dispatch]);
+
+    const handleSaveClick = useCallback(() => {
+        void (async (): Promise<void> => {
+            if (submitForm) {
+                const success = await submitForm();
+                if (success) {
+                    void dispatch(
+                        storeActions.notify({
+                            type: NotificationType.SUCCESS,
+                            message: 'Profile was updated',
+                        }),
+                    );
+                }
+            }
+        })();
+    }, [dispatch, submitForm]);
+
     return (
         <PageLayout avatarUrl="" isOnline>
-            <FormSubmitProvider>
-                <Grid className={styles.pageWrapper}>
-                    <Grid className={styles.headNavigation}>
-                        <Grid>
-                            <Typography variant="h4" className={''}>
-                                Your Profile
-                            </Typography>
-                        </Grid>
-                        <Tabs>
-                            <Tab label="Profile" href="/my/profile" />
-                            <Tab label="BSA badges" href="/my/bsa-badges" />
-                            <Tab
-                                label="Skills and projects"
-                                href="/my/skills-and-projects"
-                            />
-                            <Tab
-                                label="CV and contacts"
-                                href="/my/cv-and-contacts"
-                            />
-                            <Tab label="Hirings" href="/my/hirings" />
-                        </Tabs>
+            <Grid className={styles.pageWrapper}>
+                <Grid className={styles.headNavigation}>
+                    <Grid>
+                        <Typography variant="h4" className={''}>
+                            Your Profile
+                        </Typography>
                     </Grid>
-                    <Grid className={styles.stepBody}>
-                        <FormControl className={styles.form}>
-                            {<RouterOutlet />}
-                        </FormControl>
-                        <Grid>
-                            <Button
-                                onClick={undefined}
-                                label={'Save'}
-                                variant={'contained'}
-                                //disabled={true}
-                                className={styles.saveButton}
-                            />
-                        </Grid>
+                    <Tabs>
+                        <Tab label="Profile" href="/my/profile" />
+                        <Tab label="BSA badges" href="/my/bsa-badges" />
+                        <Tab
+                            label="Skills and projects"
+                            href="/my/skills-and-projects"
+                        />
+                        <Tab
+                            label="CV and contacts"
+                            href="/my/cv-and-contacts"
+                        />
+                        <Tab label="Hirings" href="/my/hirings" />
+                    </Tabs>
+                </Grid>
+                <Grid className={styles.stepBody}>
+                    <FormControl className={styles.form}>
+                        {<RouterOutlet />}
+                    </FormControl>
+                    <Grid>
+                        <Button
+                            onClick={handleSaveClick}
+                            label={'Save'}
+                            variant={'contained'}
+                            className={styles.saveButton}
+                        />
                     </Grid>
                 </Grid>
-            </FormSubmitProvider>
+            </Grid>
         </PageLayout>
     );
 };
