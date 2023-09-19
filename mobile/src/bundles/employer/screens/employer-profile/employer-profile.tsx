386629cc8a4bbea6ced1089ea80d3c5007411522
dobroillya <dobroillya@gmail.com>
2023-09-19 15:03:08 +0300

    bt-531: + profile form to employer screen

--- mobile/src/bundles/employer/screens/employer-profile/employer-profile.tsx
+++ mobile/src/bundles/employer/screens/employer-profile/employer-profile.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-import { Text } from '~/bundles/common/components/components';
+import { ScrollView, Text } from '~/bundles/common/components/components';
+import { TextCategory } from '~/bundles/common/enums/enums';
+import { useCallback } from '~/bundles/common/hooks/hooks';
+import { globalStyles } from '~/bundles/common/styles/styles';
+import { EmployerOnboardingForm } from '~/bundles/employer/components/components';
+
+import { styles } from './styles';
 
 const EmployerProfile: React.FC = () => {
-    return <Text>Employer Profile screen</Text>;
+    const handleEmployerDataSubmit = useCallback(() => {
+        // TODO: handle employer onboarding
+    }, []);
+
+    return (
+        <ScrollView
+            contentContainerStyle={[
+                globalStyles.defaultScreenPadding,
+                styles.container,
+            ]}
+        >
+            <Text category={TextCategory.H4} style={globalStyles.pb20}>
+                My profile
+            </Text>
+            <EmployerOnboardingForm
+                employerOnboardingData={null}
+                onSubmit={handleEmployerDataSubmit}
+            />
+        </ScrollView>
+    );
 };
 
 export { EmployerProfile };
---
+++ mobile/src/bundles/employer/screens/employer-profile/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FFFFFF',
+        flexGrow: 1,
+    },
+});
+
+export { styles };
