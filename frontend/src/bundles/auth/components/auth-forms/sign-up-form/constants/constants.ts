5490e6d5b5666b254772a9b79454538bd861ae38
JoshDagat <josuer.online.work@gmail.com>
2023-09-19 17:50:14 +0800

    bt-358: * Linter fix

--- frontend/src/bundles/auth/components/auth-forms/sign-up-form/constants/constants.ts
+++ frontend/src/bundles/auth/components/auth-forms/sign-up-form/constants/constants.ts
@@ -1,4 +1,4 @@
-import { UserRole,type UserSignUpRequestDto } from '~/bundles/users/users.js';
+import { UserRole, type UserSignUpRequestDto } from '~/bundles/users/users.js';
 
 const DEFAULT_SIGN_UP_PAYLOAD: UserSignUpRequestDto = {
     email: '',
