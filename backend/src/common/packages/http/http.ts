67632b85617d0592c18a0125556dbf5b14ff292b
JemiJe <jeremiah7480@gmail.com>
2023-09-20 20:16:33 +0300

    bt-336: + node-fetch, http package

---
+++ backend/src/common/packages/http/http.ts
@@ -0,0 +1,8 @@
+import { HttpBase } from './http.package.js';
+
+const http = new HttpBase();
+
+export { http };
+export { HttpCode, HttpHeader } from './enums/enums.js';
+export { type Http } from './types/types.js';
+export { type HttpOptions } from './types/types.js';
