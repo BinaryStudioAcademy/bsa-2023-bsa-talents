1f7a23fb232656aa49ea40fbd9c0642808f63937
JoshDagat <josuer.online.work@gmail.com>
2023-09-21 17:27:18 +0800

    bt-464: * Added sidebar variation for admin panel

--- frontend/src/bundles/common/components/page-layout/page-layout.tsx
+++ frontend/src/bundles/common/components/page-layout/page-layout.tsx
@@ -4,6 +4,7 @@ import {
     Sidebar,
 } from '~/bundles/common/components/components.js';
 
+import { useAppSelector } from '../../hooks/hooks.js';
 import styles from './styles.module.scss';
 
 type Properties = {
@@ -18,23 +19,29 @@ const PageLayout: React.FC<Properties> = ({
     isWaitingForApproval,
     isOnline,
     children,
-}) => (
-    <Grid container className={styles.pageContainer}>
-        <Sidebar />
+}) => {
+    const { role } = useAppSelector(({ auth }) => ({
+        role: auth.currentUser?.role,
+    }));
 
-        <Header
-            avatarUrl={avatarUrl}
-            isWaitingForApproval={isWaitingForApproval}
-            isOnline={isOnline}
-            className={styles.mainHeader}
-        />
+    return (
+        <Grid container className={styles.pageContainer}>
+            <Sidebar role={role} />
 
-        <Grid item className={styles.mainContainer}>
-            <Grid item className={styles.mainContent}>
-                {children}
+            <Header
+                avatarUrl={avatarUrl}
+                isWaitingForApproval={isWaitingForApproval}
+                isOnline={isOnline}
+                className={styles.mainHeader}
+            />
+
+            <Grid item className={styles.mainContainer}>
+                <Grid item className={styles.mainContent}>
+                    {children}
+                </Grid>
             </Grid>
         </Grid>
-    </Grid>
-);
+    );
+};
 
 export { PageLayout };
--- frontend/src/bundles/common/components/sidebar/sidebar.tsx
+++ frontend/src/bundles/common/components/sidebar/sidebar.tsx
@@ -1,13 +1,18 @@
-import { EmailRounded, FolderShared } from '@mui/icons-material';
+import { EmailRounded, FolderShared, Home } from '@mui/icons-material';
 
 import { Grid, Link, Logo } from '~/bundles/common/components/components.js';
 import { AppRoute } from '~/bundles/common/enums/enums.js';
 
 import { getValidClassNames } from '../../helpers/helpers.js';
 import { useCallback, useState } from '../../hooks/hooks.js';
+import { type UserRole, type ValueOf } from '../../types/types.js';
 import styles from './styles.module.scss';
 
-const menuItems = [
+type Properties = {
+    role?: ValueOf<typeof UserRole>;
+};
+
+const generalMenu = [
     {
         link: AppRoute.CANDIDATES,
         name: 'Candidates',
@@ -20,12 +25,22 @@ const menuItems = [
     },
 ];
 
-const Sidebar: React.FC = () => {
+const adminMenu = [
+    {
+        link: AppRoute.ROOT,
+        name: 'Home',
+        icon: <Home />,
+    },
+];
+
+const Sidebar: React.FC<Properties> = ({ role }) => {
     const [isSidebarVisible, setSidebarVisible] = useState(false);
 
     const handleToggleSidebar = useCallback(() => {
         setSidebarVisible(!isSidebarVisible);
     }, [isSidebarVisible]);
+
+    const menuItems = role === 'admin' ? adminMenu : generalMenu;
 
     return (
         <>
