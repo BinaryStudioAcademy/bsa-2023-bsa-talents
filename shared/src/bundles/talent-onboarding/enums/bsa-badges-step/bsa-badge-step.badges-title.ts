396f9007cdbab57b9a171cc7e943564366cb6e36
dobroillya <dobroillya@gmail.com>
2023-09-24 14:13:53 +0300

    bt-635: - typo in BsaBadgeStepBadgesTitle

--- shared/src/bundles/talent-onboarding/enums/bsa-badges-step/bsa-badge-step.badges-title.ts
+++ shared/src/bundles/talent-onboarding/enums/bsa-badges-step/bsa-badge-step.badges-title.ts
@@ -1,6 +1,6 @@
 const BsaBadgeStepBadgesTitle = {
     PROJECT_SCORE: 'You average project score',
-    LECTURE_SCORE: 'You average lecture score"',
+    LECTURE_SCORE: 'You average lecture score',
     COMMUNICATION_SCORE: 'Communication score',
     TEAM_SCORE: 'Working with team score',
     ENGLISH_LEVEL: 'Level of english',
