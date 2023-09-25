44dcb31b5839a0f15307623fd33daab84ae2f348
savinaDaria <darianna697@gmail.com>
2023-09-25 10:30:22 +0300

    bt-638: - delete redundant columns in migration

--- backend/src/migrations/20230923202930_add_hiring_info_table.ts
+++ backend/src/migrations/20230923202930_add_hiring_info_table.ts
@@ -8,27 +8,12 @@ const TableName = {
     USER_DETAILS: 'user_details',
 };
 
-const STATUS = {
-    NOT_STARTED: 'Not Started',
-    STARTED: 'Started',
-    IN_PROGRESS: 'In Progress',
-    PAID: 'paid',
-} as const;
-
 const ColumnName = {
     ID: 'id',
+    USER_ID: 'user_id',
     TALENT_ID: 'talent_id',
     COMPANY_ID: 'company_id',
-    FIRST_CONTACT_TIME: 'first_contact_time',
-    HAS_SHARED_INFO: 'has_shared_info',
-    SHARED_INFO_TIME: 'shared_info_time',
-    IS_HIRED: 'is_hired',
     HIRED_TIME: 'hired_time',
-    HIRED_SALARY: 'hired_salary',
-    HIRED_POSITION: 'hired_position',
-    STATUS: 'status',
-    FEE: 'fee',
-    IS_APPROVED: 'is_approved',
     CREATED_AT: 'created_at',
     UPDATED_AT: 'updated_at',
 };
@@ -51,30 +36,18 @@ async function up(knex: Knex): Promise<void> {
         table
             .uuid(ColumnName.TALENT_ID)
             .notNullable()
-            .references(ColumnName.ID)
+            .references(ColumnName.USER_ID)
             .inTable(TableName.USER_DETAILS)
             .onUpdate(RelationRule.CASCADE)
             .onDelete(RelationRule.CASCADE);
         table
             .uuid(ColumnName.COMPANY_ID)
             .notNullable()
-            .references(ColumnName.ID)
+            .references(ColumnName.USER_ID)
             .inTable(TableName.USER_DETAILS)
             .onUpdate(RelationRule.CASCADE)
             .onDelete(RelationRule.CASCADE);
-        table
-            .timestamp(ColumnName.FIRST_CONTACT_TIME)
-            .notNullable()
-            .defaultTo(knex.fn.now());
-        table.boolean(ColumnName.HAS_SHARED_INFO).defaultTo(false);
-        table.timestamp(ColumnName.SHARED_INFO_TIME).nullable().defaultTo(null);
-        table.boolean(ColumnName.IS_HIRED).defaultTo(false);
         table.timestamp(ColumnName.HIRED_TIME).nullable().defaultTo(null);
-        table.integer(ColumnName.HIRED_SALARY);
-        table.string(ColumnName.HIRED_POSITION);
-        table.enum(ColumnName.STATUS, Object.values(STATUS));
-        table.integer(ColumnName.FEE);
-        table.boolean(ColumnName.IS_APPROVED).defaultTo(false);
         table
             .dateTime(ColumnName.CREATED_AT)
             .notNullable()
