3a92f5faf6a5b9d4be3c5bd24c6df49a7045a5a4
JoshDagat <josuer.online.work@gmail.com>
2023-09-25 08:22:04 +0800

    bt-459: * Re-align sidebar to project standard

--- frontend/src/bundles/common/components/sidebar/sidebar.tsx
+++ frontend/src/bundles/common/components/sidebar/sidebar.tsx
@@ -7,8 +7,9 @@ import { type RootReducer } from '~/framework/store/store.package.js';
 import { getValidClassNames } from '../../helpers/helpers.js';
 import { useAppSelector, useCallback, useState } from '../../hooks/hooks.js';
 import styles from './styles.module.scss';
+import { type SideBarMenu } from './types/sidebar-menu.type.js';
 
-const generalMenu = [
+const GENERAL_MENU: SideBarMenu = [
     {
         link: AppRoute.CANDIDATES,
         name: 'Candidates',
@@ -21,7 +22,7 @@ const generalMenu = [
     },
 ];
 
-const adminMenu = [
+const ADMIN_MENU: SideBarMenu = [
     {
         link: AppRoute.ROOT,
         name: 'Home',
@@ -44,7 +45,7 @@ const Sidebar: React.FC = () => {
         setSidebarVisible(!isSidebarVisible);
     }, [isSidebarVisible]);
 
-    const menuItems = role === 'admin' ? adminMenu : generalMenu;
+    const menuItems = role === 'admin' ? ADMIN_MENU : GENERAL_MENU;
 
     return (
         <>
