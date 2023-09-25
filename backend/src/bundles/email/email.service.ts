ce038a6192ff39595efd4164ea75f4257188dc3c
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-09-25 14:42:57 +0300

    bt-333: * config vars

--- backend/src/bundles/email/email.service.ts
+++ backend/src/bundles/email/email.service.ts
@@ -3,19 +3,27 @@ import sgMail from '@sendgrid/mail';
 import { config } from '~/common/packages/packages.js';
 
 class EmailService {
+    private APIKey: string;
+    private clientUrl: string;
+    private mailSenderDomain: string;
+
     public constructor() {
-        sgMail.setApiKey(config.ENV.SEND_GRID.MAIL_API_KEY);
+        this.APIKey = config.ENV.SEND_GRID.MAIL_API_KEY;
+        this.clientUrl = config.ENV.SEND_GRID.CLIENT_URL;
+        this.mailSenderDomain = config.ENV.SEND_GRID.MAIL_SENDER_DOMAIN;
+
+        sgMail.setApiKey(this.APIKey);
     }
 
     public async sendForgotPasswordEmail(
         email: string,
         resetToken: string,
     ): Promise<void> {
-        const link = `${config.ENV.SEND_GRID.CLIENT_URL}/reset-password/${resetToken}`;
+        const link = `${this.clientUrl}/reset-password/${resetToken}`;
 
         const message = {
             to: email,
-            from: config.ENV.SEND_GRID.MAIL_SENDER_DOMAIN,
+            from: this.mailSenderDomain,
             subject: 'Reset Password',
             html: `Hello! You requested to reset your password. <a href="${link}">Click here</a> to reset your password.`,
         };
