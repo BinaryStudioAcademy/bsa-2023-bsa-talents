c55d51329a1db24e6c059be18eaa1608da3d8351
Matvey V <main.monyster@gmail.com>
2023-09-26 20:12:04 +0300

    bt-647: + link preview card

--- frontend/src/bundles/common/components/link/link.tsx
+++ frontend/src/bundles/common/components/link/link.tsx
@@ -1,14 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
-import { type AppRoute } from '~/bundles/common/enums/enums.js';
-import { type ValueOf } from '~/bundles/common/types/types.js';
-
-type RoutePattern<T extends string> = T extends `${infer Start}:${string}`
-    ? `${Start}${string}`
-    : T;
-
 type Properties = {
-    to: RoutePattern<ValueOf<typeof AppRoute>>;
+    to: string;
     children: React.ReactNode;
     className?:
         | string
