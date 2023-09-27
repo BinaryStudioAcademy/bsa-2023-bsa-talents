48277b62d4f56207fea965695ce5b45b40b0b786
Paul <rayver2012@gmail.com>
2023-09-27 20:34:22 +0300

    bt-558: + Company page

--- frontend/src/framework/store/store.package.ts
+++ frontend/src/framework/store/store.package.ts
@@ -20,6 +20,7 @@ import { reducer as bsaBadgesReducer } from '~/bundles/common/data/bsa-badges/st
 import { hardSkillsApi } from '~/bundles/common/data/hard-skills/hard-skills.js';
 import { reducer as hardSkillsReducer } from '~/bundles/common/data/hard-skills/store/hard-skills.js';
 import { AppEnvironment } from '~/bundles/common/enums/enums.js';
+import { reducer as employerReducer } from '~/bundles/employer/store/employer.js';
 import { employerOnBoardingApi } from '~/bundles/employer-onboarding/employer-onboarding.js';
 import { reducer as employerOnboardingReducer } from '~/bundles/employer-onboarding/store/employer-onboarding.js';
 import { fileUploadApi } from '~/bundles/file-upload/file-upload.js';
@@ -50,6 +51,7 @@ type RootReducer = {
     bsaBadges: ReturnType<typeof bsaBadgesReducer>;
     cabinet: ReturnType<typeof cabinetReducer>;
     chat: ReturnType<typeof chatReducer>;
+    employer: ReturnType<typeof employerReducer>;
 };
 
 type ExtraArguments = {
@@ -80,6 +82,7 @@ const combinedReducer = combineReducers({
     cabinet: cabinetReducer,
     bsaBadges: bsaBadgesReducer,
     hardSkills: hardSkillsReducer,
+    employer: employerReducer,
 });
 
 type RootState = ReturnType<typeof combinedReducer>;
