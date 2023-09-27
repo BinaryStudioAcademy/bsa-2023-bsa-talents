b9ca2670a51bad0dc146f6ee8f04b532f54446b6
dobroillya <dobroillya@gmail.com>
2023-09-27 13:52:50 +0300

    bt-714: - white space

--- mobile/src/bundles/employer/components/employer-onboarding-form/employer-onboarding-form.tsx
+++ mobile/src/bundles/employer/components/employer-onboarding-form/employer-onboarding-form.tsx
@@ -214,9 +214,7 @@ const EmployerOnboardingForm: React.FC<Properties> = ({
                     multiline={true}
                 />
             </FormField>
-            {employerOnboardingData && !isEmployerProfileScreen ? (
-                ''
-            ) : (
+            {!(employerOnboardingData && !isEmployerProfileScreen) && (
                 <Button
                     label={labelForSubmitButton}
                     onPress={handleFormSubmit}
