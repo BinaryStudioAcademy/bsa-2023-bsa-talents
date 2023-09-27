f779d61130aad74df14dac002cbcf19cea044039
nika-sirak <7nicka@gmail.com>
2023-09-27 01:02:10 +0100

    bt-667: * updatePublishedData

--- frontend/src/bundles/talent-onboarding/talent-onboarding-api.ts
+++ frontend/src/bundles/talent-onboarding/talent-onboarding-api.ts
@@ -7,7 +7,10 @@ import { HttpApiBase } from '~/framework/api/api.js';
 import { type Http } from '~/framework/http/http.js';
 import { type Storage } from '~/framework/storage/storage.js';
 
-import { type UserDetailsGeneralCustom } from './types/types.js';
+import {
+    type UserDetailsFindByUserIdRequestDto,
+    type UserDetailsGeneralCustom,
+} from './types/types.js';
 
 type Constructor = {
     baseUrl: string;
@@ -37,9 +40,9 @@ class TalentOnBoardingApi extends HttpApiBase {
     }
 
     public async updatePublishedData(
-        payload: Partial<UserDetailsGeneralCustom>,
+        payload: UserDetailsFindByUserIdRequestDto,
     ): Promise<UserDetailsGeneralCustom> {
-        const { userId = '' } = payload;
+        const { userId } = payload;
         const response = await this.load(
             this.getFullEndpoint(UserDetailsApiPath.PUBLISH, {
                 userId,
