a412fcf76e0e2a590bb81383e99eef616572e7b1
JemiJe <jeremiah7480@gmail.com>
2023-09-29 12:27:27 +0300

    bt-802: * fix update migration

--- backend/src/migrations/20230929081717_update_user_lms_data_table.ts
+++ backend/src/migrations/20230929081717_update_user_lms_data_table.ts
@@ -9,15 +9,15 @@ const ColumnName = {
 
 async function up(knex: Knex): Promise<void> {
     return knex.schema.alterTable(TABLE_NAME, (table) => {
+        table.json(ColumnName.ENGLISH).alter();
         table.renameColumn(ColumnName.ENGLISH, ColumnName.TALENT);
-        table.json(ColumnName.TALENT).alter();
     });
 }
 
 async function down(knex: Knex): Promise<void> {
     return knex.schema.alterTable(TABLE_NAME, (table) => {
-        table.renameColumn(ColumnName.TALENT, ColumnName.ENGLISH);
         table.string(ColumnName.TALENT).alter();
+        table.renameColumn(ColumnName.TALENT, ColumnName.ENGLISH);
     });
 }
 
